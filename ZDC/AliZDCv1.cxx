d579e5f2a74fb2dfe716897af2db0f9fd0838cdf
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2001-01-16 09:52:05 +0000

    Initialisation of ZDC hits

--- ZDC/AliZDCv1.cxx
+++ ZDC/AliZDCv1.cxx
@@ -15,6 +15,12 @@
 
 /*
 $Log$
+Revision 1.11  2001/01/16 07:43:33  hristov
+Initialisation of ZDC hits
+
+Revision 1.10  2000/12/14 15:20:02  coppedis
+Hits2Digits method for digitization
+
 Revision 1.9  2000/12/13 10:33:49  coppedis
 Prints only if fDebug==1
 
@@ -1249,6 +1255,7 @@ void AliZDCv1::StepManager()
   TLorentzVector s, p;
   const char *knamed;
 
+  for (j=0;j<10;j++) hits[j]=0;
 
   if((gMC->GetMedium() == fMedSensZN) || (gMC->GetMedium() == fMedSensZP) ||
      (gMC->GetMedium() == fMedSensGR) || (gMC->GetMedium() == fMedSensF1) ||
