401fff42970105150afbeb370515c29dd973822d
alibrary <alibrary@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-07-09 12:28:17 +0000

    Corrections for new MC

--- macros/loadlibs.C
+++ macros/loadlibs.C
@@ -5,34 +5,34 @@ void loadlibs ()
   // libraries required by EVGEN
   // (commented libraries are already loaded in prevoius sequence)
   //gSystem->Load("libMC");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libSTEER");
-  gSystem->Load("$(ROOTSYS)/lib/libEG"); 
-  gSystem->Load("$(ROOTSYS)/lib/libEGPythia6");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libdummypythia6");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libdummyhijing");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTHijing");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libdummyHBTP");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTHbtp");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libdummymevsim");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTMevSim");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libEVGEN");
+  gSystem->Load("libSTEER");
+  gSystem->Load("libEG"); 
+  gSystem->Load("libEGPythia6");
+  gSystem->Load("libdummypythia6");
+  gSystem->Load("libdummyhijing");
+  gSystem->Load("libTHijing");
+  gSystem->Load("libdummyHBTP");
+  gSystem->Load("libTHbtp");
+  gSystem->Load("libdummymevsim");
+  gSystem->Load("libTMevSim");
+  gSystem->Load("libEVGEN");
 
-  gSystem->Load("$(G3INSTALL)/lib/tgt_$(ALICE_TARGET)/libminicern");
-  gSystem->Load("$(ROOTSYS)/lib/libPhysics");
+  gSystem->Load("libminicern");
+  gSystem->Load("libPhysics");
 
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libCONTAINERS");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libFMD");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libMUON");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libPHOS");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libPMD");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libRICH");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libSTRUCT");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTOF");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTPC");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libTRD");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libZDC");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libITS");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libCASTOR");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libSTART");
-  gSystem->Load("$(ALICE_ROOT)/lib/tgt_$(ALICE_TARGET)/libEMCAL");
+  gSystem->Load("libCONTAINERS");
+  gSystem->Load("libFMD");
+  gSystem->Load("libMUON");
+  gSystem->Load("libPHOS");
+  gSystem->Load("libPMD");
+  gSystem->Load("libRICH");
+  gSystem->Load("libSTRUCT");
+  gSystem->Load("libTOF");
+  gSystem->Load("libTPC");
+  gSystem->Load("libTRD");
+  gSystem->Load("libZDC");
+  gSystem->Load("libITS");
+  gSystem->Load("libCRT");
+  gSystem->Load("libSTART");
+  gSystem->Load("libEMCAL");
 }
