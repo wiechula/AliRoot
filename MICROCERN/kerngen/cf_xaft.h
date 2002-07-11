a7d8cc04d67ff78dddc14709f44b9f5fc4df5e4c
alibrary <alibrary@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-07-11 17:15:24 +0000

    Adding MICROCERN

---
+++ MICROCERN/kerngen/cf_xaft.h
@@ -0,0 +1,17 @@
+/*
+* $Id$
+*
+* $Log$
+* Revision 1.1.1.1  1999/05/18 15:55:29  fca
+* AliRoot sources
+*
+* Revision 1.1.1.1  1996/02/15 17:49:17  mclareni
+* Kernlib
+*
+*
+*
+* cf#xaft.inc
+*/
+#if defined(CERNLIB_PROJSHIFT)
+#include <shift.h>
+#endif
