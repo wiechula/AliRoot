42912d1cca91eda1b9a5f8c881b772385ea7e304
iglez2 <iglez2@f7af4fe6-9843-0410-8265-dc069ae4e863>
2002-09-26 13:22:23 +0000

    Naive implementation of ProcessRun and ProcessEvent Opening/Closing of input file (fInputFileName) with FORTRAN unit 5 before/after the first call to flukam inside Init()

--- TFluka/TFluka.cxx
+++ TFluka/TFluka.cxx
@@ -15,6 +15,9 @@
 
 /*
 $Log$
+Revision 1.1.2.3  2002/09/20 15:35:51  iglez2
+Modification of LFDRTR. Value is passed to FLUKA !!!
+
 Revision 1.1.2.2  2002/09/18 14:34:44  iglez2
 Revised version with all pure virtual methods implemented
 
@@ -31,15 +34,19 @@ First commit of Fluka interface.
 #include "TFluka.h"
 #include "TCallf77.h" //For the fortran calls
 #include "Fdblprc.h"  //(DBLPRC) fluka common
-
+#include "Fiounit.h"  //(IOUNIT) fluka common
 
 #define METHODDEBUG
 
 // Fluka methods that may be needed.
 #ifndef WIN32 
 # define flukam  flukam_
+# define fluka_openinp fluka_openinp_
+# define fluka_closeinp fluka_closeinp_
 #else 
 # define flukam  FLUKAM
+# define fluka_openinp FLUKA_OPENINP
+# define fluka_closeinp FLUKA_CLOSEINP
 #endif
 
 extern "C" 
@@ -48,6 +55,8 @@ extern "C"
   // Prototypes for FLUKA functions
   //
   void type_of_call flukam(const int&);
+  void type_of_call fluka_openinp(const int&, DEFCHARA);
+  void type_of_call fluka_closeinp(const int&);
 }
 
 //
@@ -59,7 +68,7 @@ ClassImp(TFluka)
 // TFluka methods.
 //____________________________________________________________________________ 
 TFluka::TFluka()
-  :TVirtualMC()
+  :TVirtualMC(),fInputFileName("")
 { 
   //
   // Default constructor
@@ -68,7 +77,7 @@ TFluka::TFluka()
  
 //____________________________________________________________________________ 
 TFluka::TFluka(const char *title)
-  :TVirtualMC("TFluka",title)
+  :TVirtualMC("TFluka",title),fInputFileName("")
 {
 #ifdef METHODDEBUG
   cout << "==> TFluka::TFluka(" << title << ") constructor called." << endl;
@@ -81,17 +90,56 @@ TFluka::TFluka(const char *title)
 //____________________________________________________________________________ 
 void TFluka::Init() {
 #ifdef METHODDEBUG
-  cout << "==> TFluka::Init() constructor called." << endl;
+  cout << "==> TFluka::Init() called." << endl;
 #endif
 
   cout << "\t* Changing lfdrtr = (" << (GLOBAL.lfdrtr?'T':'F')
        << ") in fluka..." << endl;
   GLOBAL.lfdrtr = true;
 
+  cout << "\t* Opening file " << fInputFileName << endl;
+  const char* fname = fInputFileName;
+  fluka_openinp(lunin, PASSCHARA(fname));
+
   cout << "\t* Calling flukam..." << endl;
   flukam(0);
 
+  cout << "\t* Closing file " << fInputFileName << endl;
+  fluka_closeinp(lunin);
+
 #ifdef METHODDEBUG
-  cout << "<== TFluka::Init() constructor called." << endl;
+  cout << "<== TFluka::Init() called." << endl;
+#endif
+}
+
+//____________________________________________________________________________ 
+void TFluka::ProcessEvent() {
+#ifdef METHODDEBUG
+  cout << "==> TFluka::ProcessEvent() called." << endl;
+#endif
+
+  cout << "\t* GLOBAL.fdrtr = " << (GLOBAL.lfdrtr?'T':'F') << endl;
+  cout << "\t* Calling flukam again..." << endl;
+  flukam(0);
+
+#ifdef METHODDEBUG
+  cout << "<== TFluka::ProcessEvent() called." << endl;
+#endif
+}
+
+//____________________________________________________________________________ 
+void TFluka::ProcessRun(Int_t nevent) {
+#ifdef METHODDEBUG
+  cout << "==> TFluka::ProcessRun(" << nevent << ") called." 
+       << endl;
+#endif
+
+  cout << "\t* fdrtr = (" << (GLOBAL.lfdrtr?'T':'F') << endl;
+  cout << "\t* Calling flukam again..." << endl;
+  flukam(0);
+
+#ifdef METHODDEBUG
+  cout << "<== TFluka::ProcessRun(" << nevent << ") called." 
+       << endl;
 #endif
 }
