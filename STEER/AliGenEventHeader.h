7b394f26caa9d105d0dd77e18a8aa873e456bc2b
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-04-14 18:27:50 +0000

    Merging with v3-09-08

--- STEER/AliGenEventHeader.h
+++ STEER/AliGenEventHeader.h
@@ -9,11 +9,6 @@
 #include <TNamed.h>
 #include <TArrayF.h>
 
-// Event header base class for generator. 
-// Stores as a minimum the date, run number, event number, number of particles produced  
-// and the impact parameter
-// Author: andreas.morsch@cern.ch
-
 class AliGenEventHeader : public TNamed
 {
  public:
@@ -22,20 +17,17 @@ class AliGenEventHeader : public TNamed
   AliGenEventHeader();
   virtual ~AliGenEventHeader() {}
   // Getters
-  virtual Int_t           NProduced()         {return fNProduced;}
-  virtual Float_t         ImpactParameter()   {return fImpactParameter;}
+  virtual Int_t           NProduced()  const   {return fNProduced;}
   virtual void            PrimaryVertex(TArrayF &o) const;
   
   // Setters
   virtual void   SetNProduced(Int_t nprod)         {fNProduced=nprod;}
-  virtual void   SetImpactParameter(Float_t b)     {fImpactParameter=b;}
   virtual void   SetPrimaryVertex(const TArrayF &o);
   
 protected:
   Int_t     fNProduced;                 // Number stable or undecayed particles
-  Float_t   fImpactParameter;           // Impact Parameter
   TArrayF   fVertex;                    // Primary Vertex Position
-  ClassDef(AliGenEventHeader,1)         // Event header for primary event
+  ClassDef(AliGenEventHeader,2)         // Event header for primary event
 };
 
 #endif
