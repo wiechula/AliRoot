36d55a36117118fbea36cb54549f995988704878
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-06-11 15:51:23 +0000

    Exclamation marks for some pointers to TClonesArray

--- STEER/AliDetector.h
+++ STEER/AliDetector.h
@@ -71,8 +71,8 @@ protected:
   Int_t         fNhits;       //!Number of hits
   Int_t         fNdigits;     //!Number of digits
   Int_t         fBufferSize;  //!buffer size for Tree detector branches
-  TClonesArray *fHits;        //List of hits for one track only
-  TClonesArray *fDigits;      //List of digits for this detector
+  TClonesArray *fHits;        //!List of hits for one track only
+  TClonesArray *fDigits;      //!List of digits for this detector
   TObjArray    *fPoints;      //!Array of points for each track (all tracks in memory)
 
   AliLoader*  fLoader;//! pointer to getter for this module skowron
