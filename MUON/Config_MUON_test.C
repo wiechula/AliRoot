913b0e2c4951e31bbfcd8b5dd0ec902334176621
martinez <martinez@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-06-16 17:44:32 +0000

    Making a more general Config_MUON_test.C

--- MUON/Config_MUON_test.C
+++ MUON/Config_MUON_test.C
@@ -1,11 +1,17 @@
-void Config()
+
+// Config file test for MUON spectormeter
+// Remember to define the directory and option
+// gAlice->SetConfigFunction("Config('$HOME','box');");
+
+void Config(char directory[100]="", char option[6]="box")
 {
   //
-  // Config file for MUJON test
+  // Config file for MUON test
   // Gines MARITNEZ, Subatech, mai 2003
   // 
+
   //=====================================================================
-  // libraries required by geant321
+  //  Libraries required by geant321
   gSystem->Load("libgeant321.so");
 
   new TGeant3("C++ Interface to Geant3");
@@ -13,18 +19,17 @@ void Config()
   //=======================================================================
   //  Create the output file    
   Text_t filename[100];
-  sprintf(filename,"galice.root");
+  sprintf(filename,"%sgalice.root",directory);
   cout << ">>> Output file is " << filename << endl;   
-  cout<<">>> Config_MUON_test.C: Creating Run Loader ..."<<endl;
+  cout << ">>> Config_MUON_test.C: Creating Run Loader ..."<<endl;
   AliRunLoader* rl = AliRunLoader::Open(
 	filename, AliConfig::fgkDefaultEventFolderName, "recreate");
-  if (rl == 0x0)
-    {
-      gAlice->Fatal("Config.C","Can not instatiate the Run Loader");
-      return;
-    }
+  if (rl == 0x0) {
+    gAlice->Fatal("Config_MUON_test.C","Can not instatiate the Run Loader");
+    return;
+  }
   rl->SetCompressionLevel(2);
-  rl->SetNumberOfEventsPerFile(3);
+  rl->SetNumberOfEventsPerFile(100);
   gAlice->SetRunLoader(rl);
 
   //=======================================================================
@@ -64,7 +69,6 @@ void Config()
   geant3->SetABAN(0); //Restore 3.16 behaviour for abandoned tracks
   geant3->SetOPTI(2); //Select optimisation level for GEANT geometry searches (0,1,2)
   geant3->SetERAN(5.e-7);
-
   Float_t cut    = 1.e-3; // 1MeV cut by default
   Float_t tofmax = 1.e10;
   //             GAM ELEC NHAD CHAD MUON EBREM MUHAB EDEL MUDEL MUPA TOFMAX
@@ -72,27 +76,56 @@ void Config()
   //
   //=======================================================================
   // ************* STEERING parameters FOR ALICE SIMULATION **************
-  // --- Specify event type to be tracked through the ALICE setup
-  // --- All positions are in cm, angles in degrees, and P and E in GeV
+  // Chamber positions
   // From AliMUONConstants class we get :
-  //   Position Z (along beam) of the chambers (cm) (from AliMUONConstants class):  //      533.5,  546.5, 678.5, 693.5, 964.0, 986.0, 1251.5, 1278.5, 
-  //     1416.5, 1443.5,  1610, 1625., 1710., 1725. 
-  //   Internal Radius (cm)   
-  //     36.4,  46.2,  66.0,   80.,   80., 100., 100.    
-  //   Exterlnal Radius (cm)
-  //     183., 245., 395.,  560.,  563., 850., 900.};  
-  AliGenBox * gener = new AliGenBox(1);
-  gener->SetMomentumRange(7.,7.1);
-  gener->SetPhiRange(-180., 180.);       
-  gener->SetThetaRange(2.000,4.001);
-  gener->SetPart(13);           // Muons
-  gener->SetOrigin(0.,0., 0.);  //vertex position
-  gener->SetSigma(0.0, 0.0, 0.0);         //Sigma in (X,Y,Z) (cm) on IP position
-
-
+  //   Position Z (along beam) of the chambers (in cm) 
+  //        (from AliMUONConstants class):  
+  //    533.5,  546.5,  678.5, 693.5,  964.0, 986.0, 1251.5, 1278.5, 
+  //   1416.5, 1443.5,  1610, 1625.,  1710., 1725. 
+  //   Internal Radius (in cm)   
+  //     36.4,  46.2,  66.0,  80.,  80., 100., 100.    
+  //   External Radius (in cm)
+  //    183.,  245.,  395.,  560., 563., 850., 900.  
+  //=======================================================================
+  if (!strcmp(option,"box")) {
+    AliGenBox * gener = new AliGenBox(1);
+    gener->SetMomentumRange(7.,7.1);
+    gener->SetPhiRange(-180., 180.);         
+    gener->SetThetaRange(2.000,9.000);
+    gener->SetPart(13);           // Muons
+    gener->SetOrigin(0.,0., 0.);  //vertex position
+    gener->SetSigma(0.0, 0.0, 0.0);         //Sigma in (X,Y,Z) (cm) on IP position
+  }
+  if (!strcmp(option,"gun")) {
+    //*********************************************
+    // Example for Fixed Particle Gun             *
+    //*********************************************
+    AliGenFixed *gener = new AliGenFixed(ntracks);
+    gener->SetMomentum(10);
+    gener->SetPhiRange(0.);
+    gener->SetThetaRange(0.);
+    gener->SetOrigin(30,30,1200);//vertex position
+    gener->SetPart(13);          //GEANT particle type  13 is muons
+  }
+  if (!strcmp(option,"param")) {
+    //*******************************************************
+    // Example for J/psi or Upsilon Production from  Parameterisation *
+    //*******************************************************
+    AliGenParam *gener = new AliGenParam(1, AliGenMUONlib::kUpsilon);
+    gener->SetMomentumRange(0,999);
+    gener->SetPtRange(0,100.);
+    gener->SetPhiRange(-180, 180);
+    gener->SetYRange(2.5,4);
+    gener->SetCutOnChild(1);
+    gener->SetChildThetaRange(2.0,9);
+    gener->SetOrigin(0,0,0);          //vertex position    gener->SetSigma(0,0,0);           //Sigma in (X,Y,Z) (cm) on IP position
+    gener->SetForceDecay(kDiMuon);
+    gener->SetTrackingFlag(1);
+  }
+     
   //=============================================================
   //Specify maximum magnetic field in Tesla (neg. ==> default field)
-  gAlice->SetField(0,0) ;  //(-999,2);    
+  gAlice->SetField(2,1) ;  //(-999,2);    
 
   //=================== Alice BODY parameters =============================
   AliBODY *BODY = new AliBODY("BODY","Alice envelop");
