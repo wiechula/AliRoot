2dc062dfc49473f8bae05b38fff8619cc03eb0c0
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2003-06-24 08:48:30 +0000

    Announce changed to v3-09-Release

--- STEER/AliRun.cxx
+++ STEER/AliRun.cxx
@@ -616,7 +616,7 @@ void AliRun::Announce() const
   printf("%6s","*");printf("%64s","*\n");
 
   printf("%6s","*");
-  printf("    You are running AliRoot version v3-09-07\n");
+  printf("    You are running AliRoot version v3-09-Release\n");
 
   printf("%6s","*");
   printf("    The cvs tag for the current program is $Name$\n");
