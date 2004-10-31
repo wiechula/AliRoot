29186554f5c5bd148440384ad869ad61cd7c0c31
(no author) <(no author)@f7af4fe6-9843-0410-8265-dc069ae4e863>
2004-10-31 12:51:13 +0000

    This commit was manufactured by cvs2svn to create branch 'v4-01-Release'.

---
+++ HBTAN/AliHBTWeightashbtCorrFctn.cxx
@@ -0,0 +1,12 @@
+#include "AliHBTWeightashbtCorrFctn.h"
+
+///////////////////////////////////////////////////////
+//                                                   //
+// AliHBTWeightashbtCorrFctn.h                             //
+//                                                   //
+// Class for calculating 3D ashbt correlation        //
+// functions                                         //
+//                                                   //
+///////////////////////////////////////////////////////
+
+ClassImp(AliHBTWeightashbtCorrFctn)
---
+++ HBTAN/AliHBTashbtCorrFctn.cxx
@@ -0,0 +1,13 @@
+#include "AliHBTashbtCorrFctn.h"
+
+///////////////////////////////////////////////////////
+//                                                   //
+// AliHBTashbtCorrFctn.h                             //
+//                                                   //
+// Class for calculating 3D ashbt correlation        //
+// functions                                         //
+//                                                   //
+///////////////////////////////////////////////////////
+
+ClassImp(AliHBTashbtCorrFctn)
+
