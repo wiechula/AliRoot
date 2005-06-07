5901030c94a0046f5218c708f1ed283bc974ca30
(no author) <(no author)@f7af4fe6-9843-0410-8265-dc069ae4e863>
2005-06-07 04:43:57 +0000

    This commit was manufactured by cvs2svn to create tag 'MUON-v4-02-NewSegGeo'.

--- RICH/RICHrecLinkDef.h
+++
@@ -1,8 +0,0 @@
-#ifdef __CINT__
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-#pragma link C++ class  AliRICHMap+;
-#pragma link C++ class  AliRICHReconstructor+;
-#pragma link C++ class  AliRICHClusterFinder+;
-#endif
--- RICH/RICHsimLinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-#pragma link C++ class  AliRICHv0+;
-#pragma link C++ class  AliRICHv1+;
-#pragma link C++ class  AliGenRadioactive+;
-#pragma link C++ enum   RadioNuclei_t;
-#endif
