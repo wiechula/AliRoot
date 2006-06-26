a71fd1c3eb055bb8f3717d3e41e8f1be72aacb25
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-06-26 21:04:01 +0000

    Optimization of cuts for V0s and cascades in case of proton-proton (Ludovic)

--- ITS/AliITSReconstructor.cxx
+++ ITS/AliITSReconstructor.cxx
@@ -214,13 +214,14 @@ void AliITSReconstructor::FillESD(AliRunLoader* runLoader,
   }
   // V0 finding
   Double_t cuts[]={33,  // max. allowed chi2
-		   0.16,// min. allowed negative daughter's impact parameter 
-		   0.05,// min. allowed positive daughter's impact parameter 
-		   0.08,// max. allowed DCA between the daughter tracks
+		   0.0058,// min. allowed negative daughter's impact parameter
+		   0.0058,// min. allowed positive daughter's impact parameter
+		   6.626,// max. allowed DCA between the daughter tracks
 		   0.99,// max. allowed cosine of V0's pointing angle
-		   0.9,  // min. radius of the fiducial volume
-		   2.9   // max. radius of the fiducial volume
+		   0.2,  // min. radius of the fiducial volume
+		   999   // max. radius of the fiducial volume
   };
+
   AliV0vertexer vtxer(cuts);
   Double_t vtx[3], cvtx[6];
   esd->GetVertex()->GetXYZ(vtx);
@@ -230,14 +231,15 @@ void AliITSReconstructor::FillESD(AliRunLoader* runLoader,
 
   // cascade finding
   Double_t cts[]={33.,    // max. allowed chi2
-		  0.05,   // min. allowed V0 impact parameter 
-		  0.008,  // window around the Lambda mass 
-		  0.035,  // min. allowed bachelor's impact parameter 
-		  0.10,   // max. allowed DCA between a V0 and a track
-		  0.9985, //max. allowed cosine of the cascade pointing angle
-		  0.9,    // min. radius of the fiducial volume
-		  2.9     // max. radius of the fiducial volume
+		  0.0,   // min. allowed V0 impact parameter
+		  0.008,  // window around the Lambda mass
+		  0.0,  // min. allowed bachelor's impact parameter
+		  8.0,   // max. allowed DCA between a V0 and a track
+		  0.98, //max. allowed cosine of the cascade pointing angle
+		  0.2,    // min. radius of the fiducial volume
+		  999     // max. radius of the fiducial volume
   };
+
   AliCascadeVertexer cvtxer=AliCascadeVertexer(cts);
   cvtxer.SetVertex(vtx);
   cvtxer.V0sTracks2CascadeVertices(esd);
