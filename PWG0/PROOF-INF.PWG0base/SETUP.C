d7b727219b40b4940d75059d202e42d2232677d3
(no author) <(no author)@f7af4fe6-9843-0410-8265-dc069ae4e863>
2006-06-28 13:44:06 +0000

    This commit was manufactured by cvs2svn to create tag 'v4-04-Rev-05'.

--- PWG0/PROOF-INF.PWG0base/SETUP.C
+++
@@ -1,8 +0,0 @@
-void SETUP()
-{
-   gSystem->Load("libPWG0base");
-
-   // Set the Inlucde paths
-   gSystem->SetIncludePath("-I$ROOTSYS/include -IPWG0base");
-   gROOT->ProcessLine(".include PWG0base");
-}
