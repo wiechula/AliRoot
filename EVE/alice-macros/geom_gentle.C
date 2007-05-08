9234ddafe3a12b41ab49736749d967ad6af4e8a1
(no author) <(no author)@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-05-08 14:10:39 +0000

    This commit was manufactured by cvs2svn to create branch 'HLT-v0-6'.

--- EVE/alice-macros/geom_gentle.C
+++
@@ -1,9 +0,0 @@
-// $Id$
-
-void geom_gentle()
-{
-  TFile f("$REVESYS/alice-data/gentle_geo.root");
-  TGeoShapeExtract* gse = (TGeoShapeExtract*) f.Get("Gentle");
-  Reve::GeoShapeRnrEl::ImportShapeExtract(gse, 0);
-  f.Close();
-}
--- MUON/TestMUONPreprocessor.h
+++
@@ -1,8 +0,0 @@
-#ifndef TESTMUONPREPROCESSOR_H
-#define TESTMUONPREPROCESSOR_H
-
-class TMap;
-
-TMap* CreateDCSAliasMap();
-
-#endif
