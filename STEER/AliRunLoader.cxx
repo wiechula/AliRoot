a757cdda75e2713ce46c3e498c0eaa4556d361d1
jgrosseo <jgrosseo@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-06-27 09:45:48 +0000

    missing protection in AliRunLoader::GetEvent that caused crash when AliStack::GetEvent failed

--- STEER/AliRunLoader.cxx
+++ STEER/AliRunLoader.cxx
@@ -301,7 +301,14 @@ Int_t AliRunLoader::GetEvent(Int_t evno)
      return 2;
    }
 
-  if (fStack && fKineDataLoader->GetBaseLoader(0)->IsLoaded()) fStack->GetEvent();
+  if (fStack && fKineDataLoader->GetBaseLoader(0)->IsLoaded())
+  {
+    if (!fStack->GetEvent())
+    {
+      AliError(Form("Error occured while GetEvent for stack. Event %d",evno));
+      return 2;
+    }
+  }
   
   //Trigger data reloading in all loaders 
   TIter next(fLoaders);
