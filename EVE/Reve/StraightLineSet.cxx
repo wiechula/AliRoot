2b6c272fc787c20e8fc3e97a74cc1bd5874ed0d9
alja <alja@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-08-29 20:05:22 +0000

    Implement StraightLineSet projected class NLTSLineSet.

--- EVE/Reve/StraightLineSet.cxx
+++ EVE/Reve/StraightLineSet.cxx
@@ -8,6 +8,7 @@
 #include <TVirtualViewer3D.h>
 
 #include <TRandom.h>
+#include <Reve/NLTProjector.h>
 
 using namespace Reve;
 
@@ -18,16 +19,17 @@ using namespace Reve;
 ClassImp(StraightLineSet)
 
 StraightLineSet::StraightLineSet(const Text_t* n, const Text_t* t):
-  RenderElement(fColor),
+  RenderElement(),
   TNamed(n, t),
   fLinePlex(sizeof(Line), 4),
   fMarkerPlex(sizeof(Marker), 8),
   fRnrMarkers(kTRUE),
   fRnrLines(kTRUE)
 {
-  fColor = 4;
-  fMarkerColor = 2;
-  fMarkerStyle = 20;
+  fMainColorPtr = &fLineColor;
+  fLineColor    = 4;
+  fMarkerColor  = 2;
+  fMarkerStyle  = 20;
 }
 
 /**************************************************************************/
@@ -74,7 +76,7 @@ void StraightLineSet::Paint(Option_t* /*option*/)
 
   // Section kCore
   buff.fID           = this;
-  buff.fColor        = fColor;
+  buff.fColor        = fLineColor;
   buff.fTransparency = 0;
   buff.fLocalFrame   = kFALSE;
   buff.SetSectionsValid(TBuffer3D::kCore);
@@ -82,4 +84,66 @@ void StraightLineSet::Paint(Option_t* /*option*/)
   Int_t reqSections = gPad->GetViewer3D()->AddObject(buff);
   if (reqSections != TBuffer3D::kNone)
     Error(eH, "only direct GL rendering supported.");
+}
+
+/**************************************************************************/
+
+TClass* StraightLineSet::ProjectedClass() const
+{
+  return NLTSLineSet::Class();
+}
+
+//______________________________________________________________________
+// NLTSLineSet
+//
+
+ClassImp(NLTSLineSet)
+
+NLTSLineSet::NLTSLineSet() : StraightLineSet(), NLTProjected ()
+{}
+
+/**************************************************************************/
+
+void NLTSLineSet::SetProjection(NLTProjector* proj, NLTProjectable* model)
+{
+  NLTProjected::SetProjection(proj, model);
+
+  // copy line and marker attributes
+  * (TAttMarker*)this = * dynamic_cast<TAttMarker*>(fProjectable);
+  * (TAttLine*)this   = * dynamic_cast<TAttLine*>(fProjectable);
+}
+/**************************************************************************/
+
+void NLTSLineSet::UpdateProjection()
+{
+  NLTProjection&   proj   = * fProjector->GetProjection();
+  StraightLineSet& orig  = * dynamic_cast<StraightLineSet*>(fProjectable);
+
+  // lines
+  Int_t NL = orig.GetLinePlex().Size();
+  fLinePlex.Reset(sizeof(Line), NL);
+  Line* l;
+  Float_t x1, y1, z1;
+  Float_t x2, y2, z2;
+  VoidCPlex::iterator li(orig.GetLinePlex());
+  while (li.next()) 
+  {
+    l = (Line*) li();
+    x1 = l->fV1[0];  y1 = l->fV1[1]; z1 = l->fV1[2];
+    x2 = l->fV2[0];  y2 = l->fV2[1]; z2 = l->fV2[2];
+    proj.ProjectPoint(x1, y1, z1);
+    proj.ProjectPoint(x2, y2, z2);
+    AddLine(x1, y1, z1, x2, y2, z2);
+  }
+
+  // markers
+  Int_t NM = orig.GetMarkerPlex().Size();
+  fMarkerPlex.Reset(sizeof(Marker), NM);
+  Marker* m;
+  VoidCPlex::iterator mi(orig.GetMarkerPlex());
+  while (mi.next()) 
+  {
+    m = (Marker*) mi();
+    AddMarker(m->fLineID, m->fPos);
+  }
 }
--- EVE/Reve/StraightLineSet.h
+++ EVE/Reve/StraightLineSet.h
@@ -10,29 +10,31 @@
 #include <TQObject.h>
 #include <TAtt3D.h>
 #include <TAttMarker.h>
+#include <TAttLine.h>
 #include <TAttBBox.h>
 
 #include <Reve/Reve.h>
 #include <Reve/RenderElement.h>
+#include <Reve/NLTBases.h>
 #include <Reve/Plex.h>
 class TRandom;
 
 namespace Reve {
 
 class StraightLineSet : public RenderElement,
-		        public TNamed, public TQObject,
+                        public NLTProjectable,
+		        public TNamed, 
+                        public TQObject,
 		        public TAtt3D,
+                        public TAttLine,
                         public TAttMarker,
 		        public TAttBBox
 {
-  friend class StraightLineSetGL; 
-  friend class StraightLineSetEditor;
-
 private:
   StraightLineSet(const StraightLineSet&);            // Not implemented
   StraightLineSet& operator=(const StraightLineSet&); // Not implemented
 
-protected:
+public:
   struct Line
   {
     Float_t        fV1[3];
@@ -56,35 +58,68 @@ protected:
   };
 
 protected:
-  Color_t           fColor;
+  VoidCPlex         fLinePlex;
+  VoidCPlex         fMarkerPlex;
 
   Bool_t            fOwnLinesIds;       //Flag specifying if id-objects are owned by the QuadSet
   Bool_t            fOwnMarkersIds;     //Flag specifying if id-objects are owned by the QuadSet
-  VoidCPlex         fLinePlex;
-  Line*             fLastLine;     //!
-  VoidCPlex         fMarkerPlex;
 
   Bool_t            fRnrMarkers;
   Bool_t            fRnrLines;
+
+  Line*             fLastLine; //!          
 
 public:
   StraightLineSet(const Text_t* n="StraightLine", const Text_t* t="");
   virtual ~StraightLineSet() {}
 
-  virtual Bool_t CanEditMainColor() { return kTRUE; }
+  void    AddLine(Float_t x1, Float_t y1, Float_t z1, Float_t x2, Float_t y2, Float_t z2);
+  void    AddMarker(Int_t lineID, Float_t pos);
 
-  void AddLine(Float_t x1, Float_t y1, Float_t z1, Float_t x2, Float_t y2, Float_t z2);
-  void AddMarker(Int_t lineID, Float_t pos);
+  VoidCPlex& GetLinePlex()   { return fLinePlex;   }
+  VoidCPlex& GetMarkerPlex() { return fMarkerPlex; }
 
   virtual void ComputeBBox();
   virtual void Paint(Option_t* option="");
 
-  Color_t GetColor()    const { return fColor; };
-  void    SetColor(Color_t c) { fColor=c; }
+  virtual void SetLineColor(Color_t col) { SetMainColor(col); }
+
+  virtual void SetRnrMarkers(Bool_t x) {fRnrMarkers = x;}
+  virtual Bool_t GetRnrMarkers(){return fRnrMarkers;}
+
+  virtual void SetRnrLines(Bool_t x) {fRnrLines = x;}
+  virtual Bool_t GetRnrLines(){return fRnrLines;}
+
+
+  virtual TClass* ProjectedClass() const;
 
   ClassDef(StraightLineSet, 1);
 }; // endclass StraightLineSet
 
-}
+
+
+/**************************************************************************/
+
+class NLTSLineSet : public StraightLineSet,
+                    public NLTProjected
+{
+private:
+  NLTSLineSet(const NLTSLineSet&);            // Not implemented
+  NLTSLineSet& operator=(const NLTSLineSet&); // Not implemented
+
+protected:
+
+public:
+  NLTSLineSet();
+  virtual ~NLTSLineSet() {}
+
+  virtual void SetProjection(NLTProjector* proj, NLTProjectable* model);
+
+  virtual void UpdateProjection();
+
+  ClassDef(NLTSLineSet, 1);
+}; // endclass NLTSLineSet
+
+} // Reve
 
 #endif
