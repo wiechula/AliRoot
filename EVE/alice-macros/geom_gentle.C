e4ac1b2a8bc8c3f667399ef20b36513126a56f09
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-08-30 15:16:35 +0000

    Add missing namespace qualifier.

--- EVE/alice-macros/geom_gentle.C
+++ EVE/alice-macros/geom_gentle.C
@@ -4,7 +4,7 @@ Reve::GeoShapeRnrEl* geom_gentle()
 {
   TFile f("$REVESYS/alice-data/gentle_geo.root");
   TGeoShapeExtract* gse = (TGeoShapeExtract*) f.Get("Gentle");
-  GeoShapeRnrEl* gsre = Reve::GeoShapeRnrEl::ImportShapeExtract(gse, 0);
+  Reve::GeoShapeRnrEl* gsre = Reve::GeoShapeRnrEl::ImportShapeExtract(gse, 0);
   f.Close();
 
   return gsre;
