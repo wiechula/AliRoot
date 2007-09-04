7e85a74804dc4581e2ef064dabe4e6a12378b732
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-09-04 18:17:19 +0000

    Updated to the current status of the libraries

--- macros/loadlibs.C
+++ macros/loadlibs.C
@@ -31,21 +31,23 @@ void loadlibs ()
   gSystem->Load("libSTEERBase");
   gSystem->Load("libESD");
   gSystem->Load("libCDB");
-  gSystem->Load("libSTEER");
+  gSystem->Load("libAOD");
   gSystem->Load("libRAWDatabase");
   gSystem->Load("libRAWDatarec");
+  gSystem->Load("libSTEER");
   gSystem->Load("libRAWDatasim");
-  gSystem->Load("libEVGEN");
+
   gSystem->Load("libFASTSIM");
+  gSystem->Load("libEVGEN");
   gSystem->Load("libAliPythia6");
 
   gSystem->Load("libhijing");
   gSystem->Load("libTHijing");// AliGenHijingEventHeader needed by libZDCsim.so
 
   gSystem->Load("libSTRUCT");
-  gSystem->Load("libPHOSbase");
-  gSystem->Load("libPHOSsim");
-  gSystem->Load("libPHOSrec");
+/*   gSystem->Load("libPHOSbase"); */
+/*   gSystem->Load("libPHOSsim"); */
+/*   gSystem->Load("libPHOSrec"); */
   gSystem->Load("libMUONmapping");
   gSystem->Load("libMUONgeometry");
   gSystem->Load("libMUONcalib");
@@ -54,32 +56,32 @@ void loadlibs ()
   gSystem->Load("libMUONbase");
   gSystem->Load("libMUONsim");
   gSystem->Load("libMUONrec");
-  gSystem->Load("libMUONevaluation");
-  gSystem->Load("libFMDbase");
-  gSystem->Load("libFMDsim");
-  gSystem->Load("libFMDrec");
+/*   gSystem->Load("libMUONevaluation"); */
+/*   gSystem->Load("libFMDbase"); */
+/*   gSystem->Load("libFMDsim"); */
+/*   gSystem->Load("libFMDrec"); */
   gSystem->Load("libPMDbase");
   gSystem->Load("libPMDsim");
   gSystem->Load("libPMDrec");
   gSystem->Load("libHMPIDbase");
   gSystem->Load("libHMPIDsim");
   gSystem->Load("libHMPIDrec");
-  gSystem->Load("libT0base");
-  gSystem->Load("libT0sim");
-  gSystem->Load("libT0rec");
-  gSystem->Load("libZDCbase");
-  gSystem->Load("libZDCsim");
-  gSystem->Load("libZDCrec");
-  gSystem->Load("libACORDEbase");
-  gSystem->Load("libACORDEsim");
-  gSystem->Load("libACORDErec");
+/*   gSystem->Load("libT0base"); */
+/*   gSystem->Load("libT0sim"); */
+/*   gSystem->Load("libT0rec"); */
+/*   gSystem->Load("libZDCbase"); */
+/*   gSystem->Load("libZDCsim"); */
+/*   gSystem->Load("libZDCrec"); */
+/*   gSystem->Load("libACORDEbase"); */
+/*   gSystem->Load("libACORDEsim"); */
+/*   gSystem->Load("libACORDErec"); */
   gSystem->Load("libVZERObase");
   gSystem->Load("libVZEROsim");
   gSystem->Load("libVZEROrec");
-  gSystem->Load("libEMCALbase");
-  gSystem->Load("libEMCALsim");
-  gSystem->Load("libEMCALrec");
-  gSystem->Load("libEMCALjet");
+/*   gSystem->Load("libEMCALbase"); */
+/*   gSystem->Load("libEMCALsim"); */
+/*   gSystem->Load("libEMCALrec"); */
+/*   gSystem->Load("libEMCALjet"); */
 
   // The following lines have to be commented on Darwin
   // for the moment due to cross dependencies
@@ -90,19 +92,19 @@ void loadlibs ()
   gSystem->Load("libITSbase");
   gSystem->Load("libITSsim");
   gSystem->Load("libITSrec");
-  gSystem->Load("libTRDbase");
-  gSystem->Load("libTRDsim");
-  gSystem->Load("libTRDrec");
-  gSystem->Load("libTRDfast");
+ /*  gSystem->Load("libTRDbase"); */
+/*   gSystem->Load("libTRDsim"); */
+/*   gSystem->Load("libTRDrec"); */
+/*   gSystem->Load("libTRDfast"); */
   gSystem->Load("libTOFbase");
   gSystem->Load("libTOFsim");
   gSystem->Load("libTOFrec");
 
-  gSystem->Load("libHLTbase");
-  gSystem->Load("libAliHLTITS");
-  gSystem->Load("libAliHLTSrc");
-  gSystem->Load("libAliHLTMisc");
-  gSystem->Load("libAliHLTComp");
-  gSystem->Load("libThread");
-  gSystem->Load("libAliHLTHough");
+/*   gSystem->Load("libHLTbase"); */
+/*   gSystem->Load("libAliHLTITS"); */
+/*   gSystem->Load("libAliHLTSrc"); */
+/*   gSystem->Load("libAliHLTMisc"); */
+/*   gSystem->Load("libAliHLTComp"); */
+/*   gSystem->Load("libThread"); */
+/*   gSystem->Load("libAliHLTHough"); */
 }
