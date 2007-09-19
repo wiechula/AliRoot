3264b3282e741d7f2777b102c42767f42ade37d2
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-09-19 18:37:46 +0000

    Added empty virtual functions for event-navigation. Moved new-event-commands list and method from Alieve::Event.

--- EVE/Reve/EventBase.cxx
+++ EVE/Reve/EventBase.cxx
@@ -2,6 +2,9 @@
 
 #include "EventBase.h"
 
+#include <TObjString.h>
+#include <TCint.h>
+
 using namespace Reve;
 
 //______________________________________________________________________
@@ -11,5 +14,24 @@ using namespace Reve;
 ClassImp(EventBase)
 
 EventBase::EventBase(const Text_t* n, const Text_t* t) :
-  RenderElementList(n, t)
+  RenderElementList(n, t),
+  fNewEventCommands()
 {}
+
+/**************************************************************************/
+
+void EventBase::AfterNewEventLoaded()
+{
+  TIter next(&fNewEventCommands);
+  TObject* o;
+  while ((o = next())) {
+    TObjString* s = dynamic_cast<TObjString*>(o);
+    if (s)
+      gInterpreter->ProcessLine(s->String());
+  }
+}
+
+void EventBase::AddNewEventCommand(const Text_t* cmd)
+{
+  fNewEventCommands.Add(new TObjString(cmd));
+}
