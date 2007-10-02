9608c34d55d87b7fa6f3e12ffe9cd3b6938a6a26
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-10-02 11:44:31 +0000

    Use axis-aligned box-set for speed/memory optimization. Support secondary-selection for boxes.

--- EVE/Alieve/TPCSector3DGL.h
+++ EVE/Alieve/TPCSector3DGL.h
@@ -32,11 +32,16 @@ public:
   TPCSector3DGL();
   virtual ~TPCSector3DGL();
 
+  virtual Bool_t ShouldDLCache(const TGLRnrCtx&) const { return kFALSE; }
+
   virtual Bool_t SetModel(TObject* obj, const Option_t* opt=0);
   virtual void   SetBBox();
 
   void DrawSegmentFrame(const TPCSectorData::SegmentInfo& s,
                         Int_t botExtraPads=0, Int_t topExtraPads=0) const;
+
+  virtual Bool_t SupportsSecondarySelect() const { return kTRUE; }
+  virtual void   ProcessSelection(TGLRnrCtx & rnrCtx, TGLSelectRecord & rec);
 
   ClassDef(TPCSector3DGL, 0);
 }; // endclass TPCSector3DGL
