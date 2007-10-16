29c5179ba3d77a9570305e154291904e7401ddf5
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2007-10-16 18:03:19 +0000

    Added example line for usage of per-point integer ids.

--- EVE/alice-macros/its_hits.C
+++ EVE/alice-macros/its_hits.C
@@ -19,6 +19,7 @@ its_hits(const char *varexp    = "fX:fY:fZ",
   Reve::PointSet* points = new Reve::PointSet(form);
 
   TPointSelector ps(ht, points, varexp, selection);
+  // ps.SetSubIdExp("fTrack:fStatus");
   ps.Select();
 
   if(points->Size() == 0 && gReve->GetKeepEmptyCont() == kFALSE) {
