fb86d35e49d113bbfd5debc7f1b7d4643e06d541
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-05-21 23:18:23 +0000

    Radius for core energy is set via AliPHOSRecoParam

--- PHOS/AliPHOSReconstructor.cxx
+++ PHOS/AliPHOSReconstructor.cxx
@@ -57,8 +57,6 @@ ClassImp(AliPHOSReconstructor)
 Bool_t AliPHOSReconstructor::fgDebug = kFALSE ; 
 AliPHOSRecoParam* AliPHOSReconstructor::fgkRecoParamEmc =0;  // EMC rec. parameters
 AliPHOSRecoParam* AliPHOSReconstructor::fgkRecoParamCpv =0;  // CPV rec. parameters
-TClonesArray*     AliPHOSReconstructor::fgDigitsArray = 0;   // Array of PHOS digits
-TObjArray*        AliPHOSReconstructor::fgEMCRecPoints = 0;   // Array of EMC rec.points
 
 //____________________________________________________________________________
 AliPHOSReconstructor::AliPHOSReconstructor() :
@@ -80,8 +78,6 @@ AliPHOSReconstructor::AliPHOSReconstructor() :
   fClusterizer = new AliPHOSClusterizerv1      (fGeom);
   fTSM         = new AliPHOSTrackSegmentMakerv1(fGeom);
   fPID         = new AliPHOSPIDv1              (fGeom);
-  fgDigitsArray = new TClonesArray("AliPHOSDigit",100);
-  fgEMCRecPoints= new TObjArray(100) ;
 }
 
 //____________________________________________________________________________
@@ -92,8 +88,6 @@ AliPHOSReconstructor::AliPHOSReconstructor() :
   delete fClusterizer;
   delete fTSM;
   delete fPID;
-  delete fgDigitsArray;
-  delete fgEMCRecPoints;
 } 
 
 //____________________________________________________________________________
@@ -136,6 +130,11 @@ void AliPHOSReconstructor::FillESD(TTree* digitsTree, TTree* clustersTree,
   else 
     fPID->TrackSegments2RecParticles("") ;
 
+
+  // This function creates AliESDtracks from AliPHOSRecParticles
+  //         and
+  // writes them to the ESD
+
   TClonesArray *recParticles  = fPID->GetRecParticles();
   Int_t nOfRecParticles = recParticles->GetEntriesFast();
   
@@ -151,7 +150,8 @@ void AliPHOSReconstructor::FillESD(TTree* digitsTree, TTree* clustersTree,
     AliError("can't get the branch with the PHOS digits !");
     return;
   }
-  branch->SetAddress(&fgDigitsArray);
+  TClonesArray *digitsArray    = new TClonesArray("AliPHOSDigit",100);
+  branch->SetAddress(&digitsArray);
   branch->GetEntry(0);
 
   // Get the clusters array
@@ -162,7 +162,8 @@ void AliPHOSReconstructor::FillESD(TTree* digitsTree, TTree* clustersTree,
     return;
   }
 
-  emcbranch->SetAddress(&fgEMCRecPoints);
+  TObjArray *emcRecPoints = new TObjArray(100) ;
+  emcbranch->SetAddress(&emcRecPoints);
   emcbranch->GetEntry(0);
 
   //#########Calculate trigger and set trigger info###########
@@ -170,7 +171,7 @@ void AliPHOSReconstructor::FillESD(TTree* digitsTree, TTree* clustersTree,
   AliPHOSTrigger tr ;
   //   tr.SetPatchSize(1);//create 4x4 patches
   tr.SetSimulation(kFALSE);
-  tr.Trigger(fgDigitsArray);
+  tr.Trigger(digitsArray);
   
   Float_t maxAmp2x2  = tr.Get2x2MaxAmplitude();
   Float_t maxAmpnxn  = tr.GetnxnMaxAmplitude();
@@ -224,7 +225,7 @@ void AliPHOSReconstructor::FillESD(TTree* digitsTree, TTree* clustersTree,
   //############# Fill CaloCells ###########
   //########################################
 
-  Int_t nDigits = fgDigitsArray->GetEntries();
+  Int_t nDigits = digitsArray->GetEntries();
   Int_t idignew = 0 ;
   AliDebug(1,Form("%d digits",nDigits));
 
@@ -235,7 +236,7 @@ void AliPHOSReconstructor::FillESD(TTree* digitsTree, TTree* clustersTree,
 
   // Add to CaloCells only EMC digits with non-zero energy 
   for (Int_t idig = 0 ; idig < nDigits ; idig++) {
-    const AliPHOSDigit * dig = (const AliPHOSDigit*)fgDigitsArray->At(idig);
+    const AliPHOSDigit * dig = (const AliPHOSDigit*)digitsArray->At(idig);
     if(dig->GetId() <= knEMC && dig->GetEnergy() > 0 ){
       //printf("i %d; id %d; amp %f; time %e\n",
       //idignew,dig->GetId(),dig->GetEnergy(), dig->GetTime());
@@ -258,7 +259,7 @@ void AliPHOSReconstructor::FillESD(TTree* digitsTree, TTree* clustersTree,
     AliPHOSTrackSegment *ts    = static_cast<AliPHOSTrackSegment *>(fTSM->GetTrackSegments()
 								    ->At(rp->GetPHOSTSIndex()));
 
-    AliPHOSEmcRecPoint  *emcRP = static_cast<AliPHOSEmcRecPoint *>(fgEMCRecPoints->At(ts->GetEmcIndex()));
+    AliPHOSEmcRecPoint  *emcRP = static_cast<AliPHOSEmcRecPoint *>(emcRecPoints->At(ts->GetEmcIndex()));
     AliESDCaloCluster   *ec    = new AliESDCaloCluster() ; 
     
     Float_t xyz[3];
@@ -276,7 +277,7 @@ void AliPHOSReconstructor::FillESD(TTree* digitsTree, TTree* clustersTree,
     Double_t *fracList   = new Double_t[cellMult];
 
     for (Int_t iCell=0; iCell<cellMult; iCell++) {
-      AliPHOSDigit *digit = static_cast<AliPHOSDigit *>(fgDigitsArray->At(digitsList[iCell]));
+      AliPHOSDigit *digit = static_cast<AliPHOSDigit *>(digitsArray->At(digitsList[iCell]));
       absIdList[iCell] = (UShort_t)(digit->GetId());
       if (digit->GetEnergy() > 0)
  	fracList[iCell] = rpElist[iCell]/digit->GetEnergy();
@@ -288,10 +289,16 @@ void AliPHOSReconstructor::FillESD(TTree* digitsTree, TTree* clustersTree,
     Int_t  primMult  = 0;
     Int_t *primList =  emcRP->GetPrimaries(primMult);
 
+    Float_t energy;
+    if (fgkRecoParamEmc->Ecore2ESD())
+      energy = emcRP->GetCoreEnergy();
+    else
+      energy = rp->Energy();
+
     // fills the ESDCaloCluster
     ec->SetClusterType(AliESDCaloCluster::kPHOSCluster);
     ec->SetPosition(xyz);                       //rec.point position in MARS
-    ec->SetE(rp->Energy());                     //total particle energy
+    ec->SetE(energy);                           //total or core particle energy
     ec->SetClusterDisp(emcRP->GetDispersion()); //cluster dispersion
     ec->SetPid(rp->GetPID()) ;                  //array of particle identification
     ec->SetM02(emcRP->GetM2x()) ;               //second moment M2x
@@ -323,9 +330,11 @@ void AliPHOSReconstructor::FillESD(TTree* digitsTree, TTree* clustersTree,
     esd->AddCaloCluster(ec);
     delete ec;   
   }
-  fgDigitsArray ->Delete();
-  fgEMCRecPoints->Delete();
-  recParticles  ->Delete();
+  digitsArray ->Delete();
+  delete digitsArray;
+  emcRecPoints->Delete();
+  delete emcRecPoints;
+  recParticles->Delete();
 }
 
 //____________________________________________________________________________
