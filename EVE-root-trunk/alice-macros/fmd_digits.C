d838d9206f831ad1f381961ed5bf6ebf425299de
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-06-26 00:02:29 +0000

    Remove EVE-dev branch, it has been merged into trunk.

--- EVE-root-trunk/alice-macros/fmd_digits.C
+++
@@ -1,7 +0,0 @@
-void fmd_digits()
-{
-  gStyle->SetPalette(1);
-  AliEveFMDLoader* gFmdLoader = AliEveFMDLoader::Instance();
-  gFmdLoader->LoadDigits();
-  gEve->Redraw3D();
-}
--- EVE-root-trunk/alice-macros/fmd_hits2.C
+++
@@ -1,7 +0,0 @@
-void fmd_hits2()
-{
-  gStyle->SetPalette(1);
-  AliEveFMDLoader* gFmdLoader = AliEveFMDLoader::Instance();
-  gFmdLoader->LoadHits();
-  gEve->Redraw3D();
-}
