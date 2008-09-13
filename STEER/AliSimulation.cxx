8e63df34fe0b72b5c3af7c903b9ed58a35c9759e
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-09-13 10:23:06 +0000

    Do not delete the run loader after the hits-to-digits conversion, it is used in the QA. Additional protection if gAlice is zero

--- STEER/AliSimulation.cxx
+++ STEER/AliSimulation.cxx
@@ -1090,10 +1090,6 @@ Bool_t AliSimulation::RunHitsDigitization(const char* detectors)
     if (fStopOnError) return kFALSE;
   }
 
-  delete runLoader;
-  //PH Temporary fix to avoid interference with the PHOS loder/getter
-  //PH The problem has to be solved in more general way 09/06/05
-
   return kTRUE;
 }
 
@@ -1781,6 +1777,7 @@ Bool_t AliSimulation::RunQA()
 {
 	// run the QA on summable hits, digits or digits
 	
+  if(!gAlice) return kFALSE;
 	fQASteer->SetRunLoader(gAlice->GetRunLoader()) ;
 
 	TString detectorsw("") ;  
