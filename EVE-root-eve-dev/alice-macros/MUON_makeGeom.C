bf29d1d7f08de9e9d1573c0549187cdc919c02a4
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-10-20 18:39:24 +0000

    Remove obsolete branch.

--- EVE-root-eve-dev/alice-macros/MUON_makeGeom.C
+++
@@ -1,8 +0,0 @@
-{
-  AliMpCDB::LoadMpSegmentation2(); 
-  gAlice->Init("$ALICE_ROOT/MUON/Config.C");
-  //gAlice->Init("Config.C");
-
-  gGeoManager->Export("geometry.root");
-
-}
--- EVE-root-eve-dev/alice-macros/fmd_raw.C
+++
@@ -1,7 +0,0 @@
-void fmd_raw()
-{
-  gStyle->SetPalette(1);
-  AliEveFMDLoader* gFmdLoader = AliEveFMDLoader::Instance();
-  gFmdLoader->LoadRaw();
-  gEve->Redraw3D();
-}
