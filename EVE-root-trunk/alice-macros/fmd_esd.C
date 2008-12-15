3de43d96d236ed35320bd512e5b8caaec8d10ac2
mtadel <mtadel@f7af4fe6-9843-0410-8265-dc069ae4e863>
2008-12-15 17:20:40 +0000

    Removed obsolete branch - all development merged to trunk.

--- EVE-root-trunk/alice-macros/fmd_esd.C
+++
@@ -1,7 +0,0 @@
-void fmd_esd()
-{
-  gStyle->SetPalette(1);
-  AliEveFMDLoader* gFmdLoader = AliEveFMDLoader::Instance();
-  gFmdLoader->LoadESD();
-  gEve->Redraw3D();
-}
--- EVE-root-trunk/alice-macros/fmd_hits.C
+++
@@ -1,7 +0,0 @@
-void fmd_hits()
-{
-  gStyle->SetPalette(1);
-  AliEveFMDLoader* gFmdLoader = AliEveFMDLoader::Instance();
-  gFmdLoader->LoadHits();
-  gEve->Redraw3D();
-}
