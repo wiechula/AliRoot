daad83f79e66be01dee011322fafdb812fb8ee8c
cvetan <cvetan@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-02-13 14:12:07 +0000

    Minor correction so that to be able to use the release for LHC08d raw-data reconstruction

--- test/cosmic/rec.C
+++ test/cosmic/rec.C
@@ -17,10 +17,9 @@ void rec(const char *filename="raw.root", const Int_t mfield=1)
 
   // Set the CDB storage location
   AliCDBManager * man = AliCDBManager::Instance();
-    man->SetDefaultStorage("local://$ALICE_ROOT");
-  //man->SetDefaultStorage("alien://folder=/alice/data/2008/LHC08a/OCDB/");
-  man->SetSpecificStorage("ITS/Calib/*","local://$ALICE_ROOT");
-  
+  //    man->SetDefaultStorage("local://$ALICE_ROOT");
+  man->SetDefaultStorage("alien://folder=/alice/data/2008/LHC08d/OCDB/");
+
   // Example in case a specific CDB storage is needed
   //  man->SetSpecificStorage("ITS/Calib/MapsAnodeSDD","local://$ALICE_ROOT");
 
@@ -61,11 +60,6 @@ void rec(const char *filename="raw.root", const Int_t mfield=1)
   // TRD setting
   // Settings for the TRD Raw Reader
   AliTRDrawStreamBase::SetRawStreamVersion("TB");
-  AliTRDrawStreamTB::SetNoErrorWarning();
-  AliTRDrawStreamTB::AllowCorruptedData();
-  AliTRDrawStreamTB::DisableStackNumberChecker();
-  AliTRDrawStreamTB::DisableStackLinkNumberChecker();
-  AliTRDrawStreamTB::SetSubtractBaseline(10);
   
   // TRD reconstruction params
   AliTRDrecoParam *fTRDrecoParam = AliTRDrecoParam::GetCosmicTestParam();
