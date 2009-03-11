7b593829addc50e625006c83ba4a2a872ade43ee
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-03-11 13:18:19 +0000

    Technical fix: correct initialization (valgrind)

--- STEER/AliReconstruction.cxx
+++ STEER/AliReconstruction.cxx
@@ -2698,6 +2698,7 @@ void AliReconstruction::WriteAlignmentData(AliESDEvent* esd)
       AliESDtrack *track = esd->GetTrack(itrack);
       Int_t nsp = 0;
       Int_t idx[200];
+      for (Int_t i=0; i<200; ++i) idx[i] = -1; //PH avoid uninitialized values
       for (Int_t iDet = 3; iDet >= 0; iDet--) {// TOF, TRD, TPC, ITS clusters
           nsp += track->GetNcls(iDet);
 
