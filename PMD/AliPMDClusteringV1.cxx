f041d8fa267590e3432705466039c9f1e65b1f20
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-08-07 08:40:37 +0000

    Fix for bug #53921: Crash in PMD reconstruction - out-of-bounds array access. From rev. 34156

--- PMD/AliPMDClusteringV1.cxx
+++ PMD/AliPMDClusteringV1.cxx
@@ -956,9 +956,9 @@ void AliPMDClusteringV1::FindIsoCell(Int_t idet, Int_t ismn, Double_t celladc[][
   Int_t neiby[6] = {0,1,1,0,-1,-1};
 
 
-  for(Int_t irow = 1; irow < kMaxRow-1; irow++)
+  for(Int_t irow = 0; irow < kMaxRow; irow++)
     {
-      for(Int_t icol = 1; icol < kMaxCol-1; icol++)
+      for(Int_t icol = 0; icol < kMaxCol; icol++)
 	{
 	  if(celladc[irow][icol] > 0)
 	    {
@@ -967,8 +967,12 @@ void AliPMDClusteringV1::FindIsoCell(Int_t idet, Int_t ismn, Double_t celladc[][
 		{
 		  id1 = irow + neibx[ii];
 		  jd1 = icol + neiby[ii];
+		  if (id1 < 0) id1 = 0;
+		  if (id1 > kMaxRow-1) id1 = kMaxRow - 1;
+		  if (jd1 < 0) jd1 = 0;
+		  if (jd1 > kMaxCol-1) jd1 = kMaxCol - 1;
 		  Float_t adc = (Float_t) celladc[id1][jd1];
-		  if(adc == 0.)
+		  if(adc < 1.)
 		    {
 		      isocount++;
 		      if(isocount == kCellNeighbour)
