dfef2408b37007f6ef8a50a1e8cf0299850dc80b
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-11-03 17:17:24 +0000

    Technical fix: usind standard bit manipulation macros

--- TRD/qaRec/macros/AliTRDperformanceTrain.h
+++ TRD/qaRec/macros/AliTRDperformanceTrain.h
@@ -1,10 +1,11 @@
 #ifndef ALITRDPERFORMANCETRAIN_H
 #define ALITRDPERFORMANCETRAIN_H
 
-#define BIT(n)      (1 << (n))
-#define SETBIT(n,i) ((n) |= BIT(i))
+#include <Rtypes.h>
+/* #define BIT(n)      (1 << (n)) */
+/* #define SETBIT(n,i) ((n) |= BIT(i)) */
 #define TSTBIT(n,i) ((Bool_t)(((n) & BIT(i)) != 0))
-#define CLRBIT(n,i) ((n) &= ~BIT(i))
+/* #define CLRBIT(n,i) ((n) &= ~BIT(i)) */
 
 #define NTRDQATASKS 6
 #define NTRDCALIBTASKS 6
