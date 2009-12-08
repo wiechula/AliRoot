8089051bb40e21e2ec1d9b4c9303be2053f2a165
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-12-08 16:50:34 +0000

    Fixes for report #60189 Request to port commit 37650 to v4-17-Release (SDD reconstruction efficiency)

--- ITS/AliITSClusterFinderV2SDD.cxx
+++ ITS/AliITSClusterFinderV2SDD.cxx
@@ -450,7 +450,7 @@ Bool_t AliITSClusterFinderV2SDD::NoiseSuppress(Int_t k, Int_t sid,Int_t nzBins, 
   Int_t wW=bins[k+nzBins].GetQ();
   if(wW>tL) nLow++;
   if(wW>tH) nHigh++;
-  if(nLow<3 || nHigh<1) return kTRUE;
+  if(nLow<2 || nHigh<1) return kTRUE;
   else return kFALSE;
 }
 
