29e6955badfffb059ebd8beb3b6a7306c0ee4aec
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2009-12-08 20:29:09 +0000

    Fixes for report #60321: Request to port HLT QA update to the v4-17-Release

--- HLT/QA/AliHLTQADataMakerRec.h
+++ HLT/QA/AliHLTQADataMakerRec.h
@@ -8,7 +8,7 @@
 //* See cxx source for full Copyright notice                               *
 
 /** @file   AliHLTQADataMakerRec.h
-    @author Matthias Richter
+    @author Zhongbao Yin, Matthias Richter
     @date   2009-05-14
     @brief  Container for the HLT offline QA
 */
@@ -18,18 +18,35 @@
 class AliHLTQADataMakerRec: public AliQADataMakerRec {
 
 public:
+
+  enum HESDsType_t {kMultiplicity=0, kMultiplicityFired, kNCls, 
+		    kNClsFired, kPHLT, kPOffline, kPRatio, 
+		    kPHLTFired, kPOfflineFired, kPRatioFired,
+		    kPtHLT, kPtOffline, 
+		    kPtHLTFired, kPtOfflineFired, 
+		    kNClsPerTrkHLT, kNClsPerTrkOffline, 
+		    kNClsPerTrkHLTFired, kNClsPerTrkOfflineFired, 
+		    kPhiHLT, kPhiOffline,
+		    kPhiHLTFired, kPhiOfflineFired,
+		    kEtaHLT, kEtaOffline,
+		    kEtaHLTFired, kEtaOfflineFired};  
   
   AliHLTQADataMakerRec();
   virtual ~AliHLTQADataMakerRec();
-  
+
 private:
   /** copy constructor prohibited */
   AliHLTQADataMakerRec(const AliHLTQADataMakerRec&);   
   /** assignment operator prohibited */
   AliHLTQADataMakerRec& operator = (const AliHLTQADataMakerRec&);
 
-  virtual void   StartOfDetectorCycle();
-  virtual void   EndOfDetectorCycle(AliQAv1::TASKINDEX_t, TObjArray** list);
+  virtual void Exec(AliQAv1::TASKINDEX_t task, TObject * data);
+  virtual void StartOfDetectorCycle();
+  virtual void EndOfDetectorCycle(AliQAv1::TASKINDEX_t, TObjArray** list);
+  virtual void MakeRaws(AliRawReader * rawReader);
+  virtual void InitESDs();
+  virtual void MakeESDs(AliESDEvent * esd);
+  virtual void MakeESDs(AliESDEvent * esd, AliESDEvent* hltesd);
 
   ClassDef(AliHLTQADataMakerRec,0)  // HLT Quality Assurance Data Maker for reconstruction
 };
