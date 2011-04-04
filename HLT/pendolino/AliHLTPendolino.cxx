14ecb999d47e140d8ac7f7784c67acbd02addaa5
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-04-04 17:53:55 +0000

    Changes for #80433 Request to port bugfix of HLT pendolino to v4-20-Release. From rev. 47333,48997,49003

--- HLT/pendolino/AliHLTPendolino.cxx
+++ HLT/pendolino/AliHLTPendolino.cxx
@@ -46,7 +46,6 @@
 
 using namespace std;
 
-
 ClassImp(AliHLTPendolino)
 
 
@@ -456,7 +455,7 @@ Bool_t AliHLTPendolino::IncludeAliCDBEntryInList(const TString& entryPath) {
 
 void AliHLTPendolino::Log(const char* detector, const char* message) {
 	// logging function
-	fpLogger->log(detector, message);
+        if (fpLogger) fpLogger->log(detector, message);
 	// refer data to a Pendolino Logger, which can take care of it
 }
 
@@ -802,3 +801,8 @@ void AliHLTPendolino::Log(const char* name , const char* message, UInt_t level)
   log.LoggingVarargs(severity, "AliHLTPendolino", name , __FILE__ , __LINE__ , message);
 }
 
+// this global object is not for use, it just makes sure that
+// AliHLTPendolino can be instantiated. In case of missing virtual
+// functions a compilation error will indicate potential problems in
+// the pendolino macros
+AliHLTPendolino gAliHLTPendolino(0,"");
