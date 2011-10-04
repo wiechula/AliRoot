cd8662721e717d3ced4312e02f7199d6ed7911e4
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-10-04 09:45:03 +0000

    Changes for #87365 ZDC request to port code for TDC reco in the release. From rev. 51763,51867,51875,51876

--- ZDC/ZDCMAPPINGda.cxx
+++ ZDC/ZDCMAPPINGda.cxx
@@ -10,10 +10,9 @@ DA to write mapping for ADC modules and VME scaler
 
 Contact: Chiara.Oppedisano@to.infn.it
 Link: 
-Run Type: PHYSICS, CALIBRATION_BC, CALIBRATION_CENTRAL, 
-	  CALIBRATION_MB, CALIBRATION_SEMICENTRAL, CALIBRATION_COSMIC
+Run Type: PHYSICS
 DA Type: MON
-Number of events needed: 1 (SOD is read) 
+Number of events needed: at least 50% 
 Input Files:  none
 Output Files: ZDCChMapping.dat
 Trigger Types Used: different trigger types are used
@@ -21,6 +20,8 @@ Trigger Types Used: different trigger types are used
 */
 
 #define MAPDATA_FILE  "ZDCChMapping.dat"
+#define TDCDATA_FILE  "ZDCTDCCalib.dat"
+#define TDCHISTO_FILE "ZDCTDCHisto.root"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -32,7 +33,15 @@ Trigger Types Used: different trigger types are used
 #include <daqDA.h>
 
 //ROOT
+#include <TROOT.h>
+#include <TPluginManager.h>
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TProfile.h>
+#include <TF1.h>
 #include <TFile.h>
+#include <TFitter.h>
+#include "TMinuitMinimizer.h"
 
 //AliRoot
 #include <AliRawReaderDate.h>
@@ -41,14 +50,68 @@ Trigger Types Used: different trigger types are used
 
 int main(int argc, char **argv) {
 
-  const Char_t* tableSOD[]  = {"ALL", "no", "SOD", "all", NULL, NULL};
-  monitorDeclareTable(const_cast<char**>(tableSOD));
+  gROOT->GetPluginManager()->AddHandler("TVirtualStreamerInfo",
+					"*",
+					"TStreamerInfo",
+					"RIO",
+					"TStreamerInfo()"); 
+
+  TMinuitMinimizer m; 
+  gROOT->GetPluginManager()->AddHandler("ROOT::Math::Minimizer", "Minuit","TMinuitMinimizer",
+      "Minuit", "TMinuitMinimizer(const char *)");
+  TVirtualFitter::SetDefaultFitter("Minuit");
+
+  //const Char_t* tableSOD[]  = {"ALL", "no", "SOD", "all", NULL, NULL};
+  //monitorDeclareTable(const_cast<char**>(tableSOD));
   
-  int status = 0;
+  int status=0, nphys=0;
   int const kNModules = 10;
   int const kNChannels = 24;
-  int const kNScChannels = 32;
-
+  int const kNScChannels = 32;  
+  int const kZDCTDCGeo=4;
+  
+  int itdc=0, iprevtdc=-1, ihittdc=0;
+  float tdcData[6], tdcL0=-999.;	
+  for(int ij=0; ij<6; ij++) tdcData[ij]=-999.;
+  
+  Int_t iMod=-1;
+  Int_t modGeo[kNModules], modType[kNModules],modNCh[kNModules];
+  for(Int_t kl=0; kl<kNModules; kl++){
+     modGeo[kl]=modType[kl]=modNCh[kl]=0;
+  }
+  
+  Int_t ich=0;
+  Int_t adcMod[2*kNChannels], adcCh[2*kNChannels], sigCode[2*kNChannels];
+  Int_t det[2*kNChannels], sec[2*kNChannels];
+  for(Int_t y=0; y<2*kNChannels; y++){
+    adcMod[y]=adcCh[y]=sigCode[y]=det[y]=sec[y]=0;
+  }
+  
+  Int_t iScCh=0;
+  Int_t scMod[kNScChannels], scCh[kNScChannels], scSigCode[kNScChannels];
+  Int_t scDet[kNScChannels], scSec[kNScChannels];
+  for(Int_t y=0; y<kNScChannels; y++){
+    scMod[y]=scCh[y]=scSigCode[y]=scDet[y]=scSec[y]=0;
+  }
+      
+  Int_t itdcCh=0;
+  Int_t tdcMod[kNScChannels], tdcCh[kNScChannels], tdcSigCode[kNScChannels];
+  Int_t tdcDet[kNScChannels], tdcSec[kNScChannels];
+  for(Int_t y=0; y<kNScChannels; y++){
+    tdcMod[y]=tdcCh[y]=tdcSigCode[y]=tdcDet[y]=tdcSec[y]=-1;
+  }
+ 
+  TH1F * hTDC[6];
+  char ntdchist[20];
+  for(Int_t it=0; it<6; it++){
+    if(it==0)      hTDC[it] = new TH1F("TDCZNC", "TDC ZNC", 200, -200., 200.);
+    else if(it==1) hTDC[it] = new TH1F("TDCZNA", "TDC ZNA", 200, -200., 200.);
+    else if(it==2) hTDC[it] = new TH1F("TDCZPC", "TDC ZPC", 200, -200., 200.);
+    else if(it==3) hTDC[it] = new TH1F("TDCZPA", "TDC ZPA", 200, -200., 200.);
+    else if(it==4) hTDC[it] = new TH1F("TDCZEM1","TDC ZEM1",200, -200., 200.);
+    else if(it==5) hTDC[it] = new TH1F("TDCZEM2","TDC ZEM2",200, -200., 200.);
+  }
+  
   /* log start of process */
   printf("\n ZDC MAPPING program started\n");  
 
@@ -84,7 +147,7 @@ int main(int argc, char **argv) {
 
     Int_t iev = 0;
     Bool_t sodRead = kFALSE;
-    while(!sodRead && iev<1000){
+    while(!sodRead){
  
       /* check shutdown condition */
       if (daqDA_checkShutdown()) {break;}
@@ -118,34 +181,6 @@ int main(int argc, char **argv) {
       
       if(eventT==START_OF_DATA){
 	  	
-      
-        Int_t iMod=-1;
-        Int_t modGeo[kNModules], modType[kNModules],modNCh[kNModules];
-        for(Int_t kl=0; kl<kNModules; kl++){
-      	   modGeo[kl]=modType[kl]=modNCh[kl]=0;
-        }
-        
-        Int_t ich=0;
-        Int_t adcMod[2*kNChannels], adcCh[2*kNChannels], sigCode[2*kNChannels];
-        Int_t det[2*kNChannels], sec[2*kNChannels];
-        for(Int_t y=0; y<2*kNChannels; y++){
-          adcMod[y]=adcCh[y]=sigCode[y]=det[y]=sec[y]=-1;
-        }
-      
-        Int_t iScCh=0;
-        Int_t scMod[kNScChannels], scCh[kNScChannels], scSigCode[kNScChannels];
-        Int_t scDet[kNScChannels], scSec[kNScChannels];
-        for(Int_t y=0; y<kNScChannels; y++){
-          scMod[y]=scCh[y]=scSigCode[y]=scDet[y]=scSec[y]=-1;
-        }
-      
-        Int_t itdcCh=0;
-        Int_t tdcMod[kNScChannels], tdcCh[kNScChannels], tdcSigCode[kNScChannels];
-        Int_t tdcDet[kNScChannels], tdcSec[kNScChannels];
-        for(Int_t y=0; y<kNScChannels; y++){
-          tdcMod[y]=tdcCh[y]=tdcSigCode[y]=tdcDet[y]=tdcSec[y]=-1;
-        }
-	
 	rawStreamZDC->SetSODReading(kTRUE);
 	
 	// --------------------------------------------------------
@@ -214,13 +249,42 @@ int main(int argc, char **argv) {
 	}
         fclose(mapFile4Shuttle);
       }// SOD event
-      else{ 
-        if(sodRead){
-	  printf("\t SOR read -> exiting from DA\n");
-	  break;
+      else if(eventT==PHYSICS_EVENT){ 
+
+	rawStreamZDC->SetSODReading(kTRUE);
+
+  	// ----- Setting ch. mapping -----
+	for(Int_t jk=0; jk<2*kNChannels; jk++){
+	  rawStreamZDC->SetMapADCMod(jk, adcMod[jk]);
+	  rawStreamZDC->SetMapADCCh(jk, adcCh[jk]);
+	  rawStreamZDC->SetMapADCSig(jk, sigCode[jk]);
+	  rawStreamZDC->SetMapDet(jk, det[jk]);
+	  rawStreamZDC->SetMapTow(jk, sec[jk]);
 	}
-	else continue;
-      }
+	
+  	while(rawStreamZDC->Next()){
+          if(rawStreamZDC->GetADCModule()==kZDCTDCGeo && rawStreamZDC->IsZDCTDCDatum()==kTRUE){
+             //
+	     itdc = rawStreamZDC->GetChannel(); 
+	     if((itdc>=8 && itdc<=13) || itdc==15){
+               if(itdc==iprevtdc) ihittdc++;
+               else ihittdc=0;
+               iprevtdc=itdc;
+               if(ihittdc<1) tdcData[itdc-8] = 0.025*rawStreamZDC->GetZDCTDCDatum();
+	       //
+	       if(itdc==15 && ihittdc<1){
+	         tdcL0 = 0.025*rawStreamZDC->GetZDCTDCDatum();
+       	         for(int ic=0; ic<6; ic++){
+		   if(tdcData[ic]!=-999. && tdcL0!=-999.) hTDC[ic]->Fill(tdcData[ic]-tdcL0);
+		 }
+	       }
+	     }
+	  }
+        }
+	
+	nphys++;
+	
+      }//(if PHYSICS_EVENT) 
       
       iev++; 
 
@@ -229,11 +293,67 @@ int main(int argc, char **argv) {
     }    
       
   }
+  printf(" \n # of physics events analyzed = %d\n\n", nphys);
+
+  /* Analysis of the histograms */
+  //
+  FILE *fileShuttle;
+  fileShuttle = fopen(TDCDATA_FILE,"w");
+  //
+  Float_t xUp=0., xLow=0., deltaX=0;
+  Int_t binMax=0, nBinsx=0;
+  Float_t mean[6], sigma[6];
+  TF1 *fitfun[6];
+  for(Int_t k=0; k<6; k++){
+    if(hTDC[k]->GetEntries()!=0){
+       binMax = hTDC[k]->GetMaximumBin();
+       if(binMax<=1){
+         printf("\n WARNING! Something wrong with det %d histo \n\n", k);
+         continue;
+       }
+       // 
+       xUp = (hTDC[k]->GetXaxis())->GetXmax();
+       xLow = (hTDC[k]->GetXaxis())->GetXmin();
+       deltaX = xUp-xLow;
+       nBinsx = (hTDC[k]->GetXaxis())->GetNbins();
+       //printf(" xMax = %f\n", xLow+binMax*deltaX/nBinsx);
+       hTDC[k]->Fit("gaus","Q","",xLow+binMax*deltaX/nBinsx*0.6,xLow+binMax*deltaX/nBinsx*1.24);
+       fitfun[k] = hTDC[k]->GetFunction("gaus");
+       mean[k] = (Float_t) (fitfun[k]->GetParameter(1));
+       sigma[k] = (Float_t) (fitfun[k]->GetParameter(2));
+       //printf("\t Mean value from fit = %1.2f\n", mean[k]);
+       //
+       fprintf(fileShuttle,"\t%f\t%f\n",mean[k], sigma[k]);
+     }
+     else printf("  WARNING! TDC histo %d has no entries and can't be processed!\n",k);
+  }
+  //						       
+  fclose(fileShuttle);
+  
+  TFile *histofile = new TFile(TDCHISTO_FILE,"RECREATE");
+  histofile->cd();
+  for(int k=0; k<6; k++) hTDC[k]->Write();						       
+  histofile->Close();
+  //
+  for(Int_t j=0; j<6; j++) delete hTDC[j];
   
   /* store the result files on FES */
+  // [1] File with mapping
   status = daqDA_FES_storeFile(MAPDATA_FILE, "MAPPING");
   if(status){
     printf("Failed to export file : %d\n",status);
+    return -1;
+  }
+  // [2] File with TDC data
+  status = daqDA_FES_storeFile(TDCDATA_FILE, "TDCDATA");
+  if(status){
+    printf("Failed to export TDC data file to DAQ FES\n");
+    return -1;
+  }
+  // [3] File with TDC histos
+  status = daqDA_FES_storeFile(TDCHISTO_FILE, "TDCHISTOS");
+  if(status){
+    printf("Failed to export TDC histos file to DAQ FES\n");
     return -1;
   }
 
