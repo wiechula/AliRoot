c21b9f168d5208c4fc10e4250ae25b9544675c35
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2011-11-23 20:31:40 +0000

    Changes for #89027 Request to port a fix r/53002: Small memory leak in AliQADataMaker

--- STEER/STEER/AliQADataMaker.cxx
+++ STEER/STEER/AliQADataMaker.cxx
@@ -351,6 +351,7 @@ Int_t AliQADataMaker::SetEventTrigClasses(const char* triggers)
   int nTrigs = tarr->GetEntriesFast();
   if (!nTrigs) {
     AliWarningClass("The string for event triggers is empty");
+    delete tarr;
     return 0;
   }
   int nTrClasses = 0;
