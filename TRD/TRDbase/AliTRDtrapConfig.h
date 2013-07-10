e05f46640f6ee5d3767c54f051b8ded6189e4269
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-07-10 14:03:10 +0000

    Root6 changes: making the nested classes public

--- TRD/TRDbase/AliTRDtrapConfig.h
+++ TRD/TRDbase/AliTRDtrapConfig.h
@@ -521,7 +521,6 @@ class AliTRDtrapConfig : public TNamed
   static const Int_t fgkDbankStartAddress = 0xf000; // start address in TRAP GIO
   static const Int_t fgkDbankWords = 0x0100;        // number of words in DBANK
 
- protected:
   void InitRegs();
 
   class AliTRDtrapValue : public TObject {
@@ -611,6 +610,7 @@ class AliTRDtrapConfig : public TNamed
     ClassDef(AliTRDtrapDmemWord, 1);
   };
 
+ protected:
   // configuration registers
   AliTRDtrapRegister fRegisterValue[kLastReg];  // array of TRAP register values in use
 
