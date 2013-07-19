5db2b6858b8e97eb7bfe26afaf18dc4c0991059f
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-07-19 20:40:51 +0000

    Fixes for Root6

--- EVGEN/AliGenEMCocktail.cxx
+++ EVGEN/AliGenEMCocktail.cxx
@@ -94,7 +94,7 @@ void AliGenEMCocktail::CreateCocktail()
   genpizero = new AliGenParam(fNPart, new AliGenEMlib(), AliGenEMlib::kPizero, "DUMMY");
   AddSource2Generator(namePizero,genpizero);
   TF1 *fPtPizero = genpizero->GetPt();
-  fYieldArray[kGenPizero] = fPtPizero->Integral(fPtMin,fPtMax,(Double_t *) 0x0,1.e-6);
+  fYieldArray[kGenPizero] = fPtPizero->Integral(fPtMin,fPtMax,1.e-6);
 // eta  
   AliGenParam * geneta=0;
   Char_t nameEta[10];    
@@ -102,7 +102,7 @@ void AliGenEMCocktail::CreateCocktail()
   geneta = new AliGenParam(fNPart, new AliGenEMlib(), AliGenEMlib::kEta, "DUMMY");
   AddSource2Generator(nameEta,geneta);
   TF1 *fPtEta = geneta->GetPt();
-  fYieldArray[kGenEta] = fPtEta->Integral(fPtMin,fPtMax,(Double_t *) 0x0,1.e-6);
+  fYieldArray[kGenEta] = fPtEta->Integral(fPtMin,fPtMax,1.e-6);
 // rho  
   AliGenParam * genrho=0;
   Char_t nameRho[10];    
@@ -110,7 +110,7 @@ void AliGenEMCocktail::CreateCocktail()
   genrho = new AliGenParam(fNPart, new AliGenEMlib(), AliGenEMlib::kRho, "DUMMY");
   AddSource2Generator(nameRho,genrho);
   TF1 *fPtRho = genrho->GetPt();
-  fYieldArray[kGenRho] = fPtRho->Integral(fPtMin,fPtMax,(Double_t *) 0x0,1.e-6);
+  fYieldArray[kGenRho] = fPtRho->Integral(fPtMin,fPtMax,1.e-6);
 // omega
   AliGenParam * genomega=0;
   Char_t nameOmega[10];    
@@ -118,7 +118,7 @@ void AliGenEMCocktail::CreateCocktail()
   genomega = new AliGenParam(fNPart, new AliGenEMlib(), AliGenEMlib::kOmega, "DUMMY");
   AddSource2Generator(nameOmega,genomega);
   TF1 *fPtOmega = genomega->GetPt();
-  fYieldArray[kGenOmega] = fPtOmega->Integral(fPtMin,fPtMax,(Double_t *) 0x0,1.e-6);
+  fYieldArray[kGenOmega] = fPtOmega->Integral(fPtMin,fPtMax,1.e-6);
 // etaprime
   AliGenParam * genetaprime=0;
   Char_t nameEtaprime[10];    
@@ -126,7 +126,7 @@ void AliGenEMCocktail::CreateCocktail()
   genetaprime = new AliGenParam(fNPart, new AliGenEMlib(), AliGenEMlib::kEtaprime, "DUMMY");
   AddSource2Generator(nameEtaprime,genetaprime);
   TF1 *fPtEtaprime = genetaprime->GetPt();
-  fYieldArray[kGenEtaprime] = fPtEtaprime->Integral(fPtMin,fPtMax,(Double_t *) 0x0,1.e-6);
+  fYieldArray[kGenEtaprime] = fPtEtaprime->Integral(fPtMin,fPtMax,1.e-6);
 // phi  
   AliGenParam * genphi=0;
   Char_t namePhi[10];    
@@ -134,7 +134,7 @@ void AliGenEMCocktail::CreateCocktail()
   genphi = new AliGenParam(fNPart, new AliGenEMlib(), AliGenEMlib::kPhi, "DUMMY");
   AddSource2Generator(namePhi,genphi);
   TF1 *fPtPhi = genphi->GetPt();
-  fYieldArray[kGenPhi] = fPtPhi->Integral(fPtMin,fPtMax,(Double_t *) 0x0,1.e-6);
+  fYieldArray[kGenPhi] = fPtPhi->Integral(fPtMin,fPtMax,1.e-6);
 }
 
 //-------------------------------------------------------------------
