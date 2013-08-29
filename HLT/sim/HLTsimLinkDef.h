3e7e8c257d85c32561f3b179dc1a578232e9af62
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-08-29 20:14:41 +0000

    Adding HLT/sim

---
+++ HLT/sim/HLTsimLinkDef.h
@@ -0,0 +1,14 @@
+#ifndef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliHLTSimulation+;
+#pragma link C++ class AliHLTAgentSim+;
+#pragma link C++ class AliHLTOUTComponent+;
+
+#endif // __CINT__
+//
+// EOF
+//
