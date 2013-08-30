b10e77deb2489563ca93f2cca8e120dbcb515662
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-08-30 15:30:17 +0000

    Adding HLT/QA

---
+++ HLT/QA/HLTqadmLinkDef.h
@@ -0,0 +1,17 @@
+#ifndef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliHLTQADataMakerSim+;
+#pragma link C++ class AliHLTQADataMakerBase+;
+#pragma link C++ class AliHLTQADataMakerRec+;
+#pragma link C++ class AliHLTQAAgent+;
+#pragma link C++ class AliHLTTPCQADataMaker+;
+#pragma link C++ class AliHLTQAChecker+;
+
+#endif // __CINT__
+//
+// EOF
+//
