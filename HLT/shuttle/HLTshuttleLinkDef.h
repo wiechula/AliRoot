a2cbccb403cc15f1722f38f09aca0110e333a642
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-08-30 18:21:32 +0000

    Adding HLT/shuttle

---
+++ HLT/shuttle/HLTshuttleLinkDef.h
@@ -0,0 +1,13 @@
+// Auto generated file - do not edit
+#ifndef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliHLTPreprocessor+;
+
+#endif // __CINT__
+//
+// EOF
+//
