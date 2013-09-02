a65b12fab1a48beecbb534f18c4d2e088c91b483
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-09-02 09:26:51 +0000

    Adding HLT/RCU and HLT/TPCLib

---
+++ HLT/RCU/AliHLTRCULinkDef.h
@@ -0,0 +1,17 @@
+// Auto generated file - do not edit
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class AliHLTAltroChannelSelectorComponent+;
+#pragma link C++ class AliHLTAltroEncoder+;
+#pragma link C++ class AliHLTAltroGenerator+;
+#pragma link C++ class AliHLTAltroTimebinAverageComponent+;
+#pragma link C++ class AliHLTRCUAgent+;
+
+#endif // __CINT__
+//
+// EOF
+//
