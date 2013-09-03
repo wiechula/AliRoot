ac2a3f7313a0f620c5222dbf031626015d4ca7c7
hristov <hristov@f7af4fe6-9843-0410-8265-dc069ae4e863>
2013-09-03 15:14:39 +0000

    Adding more HLT libraries

--- HLT/MUON/AliHLTMUONLinkDef.h
+++ HLT/MUON/AliHLTMUONLinkDef.h
@@ -31,11 +31,11 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
-#pragma link off all typedefs;
+//#pragma link off all typedefs;
 
 #pragma link C++ nestedclass;
 #pragma link C++ nestedtypedef;
-#pragma link C++ nestedfunction;
+//#pragma link C++ nestedfunction;
 
 #pragma link C++ class AliHLTMUONAgent+;
 #pragma link C++ class AliHLTMUONConstants+;
