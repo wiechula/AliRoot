8f765acf08be6eca61290831b16c9a81b99bc3f7
atimmins <anthony.timmins@cern.ch>
2015-09-20 16:04:34 -0500

    First changes needed for flat esd to be read

--- STEER/STEERBase/AliVEvent.h
+++ STEER/STEERBase/AliVEvent.h
@@ -35,6 +35,7 @@ class AliVMFT;   // AU
 class AliESDkink;
 class AliESDv0;
 class AliESDVertex;
+class AliESDVZERO;
 class AliVTrack;
 
 class AliVEvent : public TObject {
@@ -141,6 +142,7 @@ public:
   virtual ULong64_t GetTriggerMask() const = 0;
   virtual UChar_t   GetTriggerCluster() const = 0;
   virtual TString   GetFiredTriggerClasses() const = 0;
+  virtual Bool_t    IsTriggerClassFired(const char *name) const {return 0;}
   virtual Double_t  GetZDCN1Energy() const = 0;
   virtual Double_t  GetZDCP1Energy() const = 0;
   virtual Double_t  GetZDCN2Energy() const = 0;
@@ -216,9 +218,12 @@ public:
   virtual Int_t            GetV0(AliESDv0&, Int_t /*iv0*/) const {return 0;}
   virtual UInt_t           GetTimeStamp() const { return 0; }
   virtual AliVfriendEvent* FindFriend() const { return 0; }
+  virtual void             SetFriendEvent( AliVfriendEvent* ) {}
   virtual UInt_t           GetEventSpecie() const { return 0; }
   virtual AliESDkink*      GetKink(Int_t /*i*/) const { return NULL; }
   virtual Int_t            GetNumberOfKinks() const { return 0; }
+ 
+  virtual Int_t GetVZEROData( AliESDVZERO & ) const {return -1;}
 
   // Primary vertex
   virtual const AliVVertex   *GetPrimaryVertex() const {return 0x0;}
@@ -231,13 +236,10 @@ public:
   virtual Int_t GetPrimaryVertexSPD( AliESDVertex & ) const {return 0;}
   virtual Int_t GetPrimaryVertexTracks( AliESDVertex & ) const {return 0;}
 
-  // event status
-  virtual Bool_t IsIncompleteDAQ() {return kFALSE;}
-
-
   virtual void ConnectTracks() {}
   virtual EDataLayoutType GetDataLayoutType() const = 0;
   const char* Whoami();
+  virtual ULong64_t  GetSize()  const {return 0;}
 
   ClassDef(AliVEvent, 3)  // base class for AliEvent data
 };
