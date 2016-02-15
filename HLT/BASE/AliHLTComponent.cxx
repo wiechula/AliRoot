8054be89caa02f49c0c186a65b62b6dd0db932f1
mkrzewic <mikolaj.krzewicki@cern.ch>
2016-02-15 12:11:38 +0100

    when cleaning up TCollection, make sure we are owner
    this way we delete also the contents, this does not work recursively!

--- HLT/BASE/AliHLTComponent.cxx
+++ HLT/BASE/AliHLTComponent.cxx
@@ -1292,6 +1292,11 @@ int AliHLTComponent::CleanupInputObjects()
     // pops up again.
     if (pObj &&
 	(!TObject::GetObjectStat() || gObjectTable->PtrIsValid(pObj))) {
+      TCollection* coll = dynamic_cast<TCollection*>(pObj);
+      if (coll)
+      {
+        coll->SetOwner(kTRUE);
+      }
       delete pObj;
     }
   }
