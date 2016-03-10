80578e79ef9f1dbd856292c673c3e92d1fb1327a
hupereir <pereira@hep.saclay.cea.fr>
2016-03-10 11:32:33 +0100

    Several changes to handling the pre-cluster list: - keep an internal "running" pointer to the buffer for filling the structure rather than rely on members (should be more robust) - use a std::vector, rather than pointer to vector, to store pre-cluster - added stream operators.

--- HLT/MUON/AliHLTMUONPreClustersBlock.cxx
+++ HLT/MUON/AliHLTMUONPreClustersBlock.cxx
@@ -28,91 +28,105 @@
 
 //_________________________________________________________________________________________________
 AliHLTMUONPreClustersBlock::AliHLTMUONPreClustersBlock() :
-AliHLTLogging(),
-fSize(0),
-fWriteMode(false),
-fSize4PreCl(0),
-fSize4Digit(0),
-fCurrentSize(1),
-fNPreClusters(NULL),
-fLastNDigits(NULL),
-fLastDigit(NULL),
-fPreClusters(NULL)
+    AliHLTLogging(),
+    fSize(0),
+    fWriteMode(false),
+    fSize4PreCl(0),
+    fSize4Digit(0),
+    fCurrentSize(1),
+    fNPreClusters(0x0),
+    fLastNDigits(0x0),
+    fLastDigit(0x0)
 {
   // default constructor
 }
 
 //_________________________________________________________________________________________________
 AliHLTMUONPreClustersBlock::AliHLTMUONPreClustersBlock(void *buffer, AliHLTUInt32_t size, bool write) :
-AliHLTLogging(),
-fSize(size),
-fWriteMode(write),
-fSize4PreCl((size > fgkSizeOfUShort+fgkSizeOfDigit) ? size - fgkSizeOfUShort - fgkSizeOfDigit : 0),
-fSize4Digit((size > fgkSizeOfDigit) ? size - fgkSizeOfDigit : 0),
-fCurrentSize(size+1),
-fNPreClusters(NULL),
-fLastNDigits(NULL),
-fLastDigit(NULL),
-fPreClusters(NULL)
+  AliHLTLogging(),
+  fSize(size),
+  fWriteMode(write),
+  fSize4PreCl((size > fgkSizeOfUShort+fgkSizeOfDigit) ? size - fgkSizeOfUShort - fgkSizeOfDigit : 0),
+  fSize4Digit((size > fgkSizeOfDigit) ? size - fgkSizeOfDigit : 0),
+  fCurrentSize(size+1),
+  fNPreClusters(0x0),
+  fLastNDigits(0x0),
+  fLastDigit(0x0)
 {
-  // constructor
 
-  assert(buffer != NULL);
-
-  if (size >= fgkSizeOfUShort) {
-    fCurrentSize = fgkSizeOfUShort;
-    fNPreClusters = reinterpret_cast<AliHLTUInt16_t*>(buffer);
-    if (fWriteMode) *fNPreClusters = 0;
-    else ReadBuffer();
-  } else if (fWriteMode) {
-    HLTError("The buffer is too small to store the data block.");
-  } else {
-    HLTError("The buffer is too small to contain the data block.");
-  }
+  /// constructor
+  // initialization is the same as in the "reset" method
+  Reset( buffer, size, write );
 
 }
 
 //_________________________________________________________________________________________________
 AliHLTMUONPreClustersBlock::~AliHLTMUONPreClustersBlock()
-{
-  // destructor
-
-  delete fPreClusters;
-  fPreClusters = NULL;
-
-}
+{}
 
 //_________________________________________________________________________________________________
 int AliHLTMUONPreClustersBlock::Reset(void *buffer, AliHLTUInt32_t size, bool write)
 {
-  /// initialize the internal structure in write mode
 
-  assert(buffer != NULL);
+  /// initialize the internal structure in write mode
+  assert(buffer != 0x0);
 
   int status = 0;
 
+  // store buffer
+  fBuffer = buffer;
+
+  // store buffer size
   fSize = size;
+
+  // store write mode
   fWriteMode = write;
+
+  // reset
   fSize4PreCl = (size > fgkSizeOfUShort+fgkSizeOfDigit) ? size - fgkSizeOfUShort - fgkSizeOfDigit : 0;
   fSize4Digit = (size > fgkSizeOfDigit) ? size - fgkSizeOfDigit : 0;
-  fLastNDigits = NULL;
-  fLastDigit = NULL;
-  if (fPreClusters) fPreClusters->clear();
+  fLastNDigits = 0x0;
+  fLastDigit = 0x0;
+  fPreClusters.clear();
 
   if (size >= fgkSizeOfUShort) {
-    fCurrentSize = fgkSizeOfUShort;
-    fNPreClusters = reinterpret_cast<AliHLTUInt16_t*>(buffer);
-    if (fWriteMode) *fNPreClusters = 0;
-    else status = ReadBuffer();
+
+    // store number of precluster
+    fNPreClusters = reinterpret_cast<AliHLTUInt16_t*>(fBuffer);
+
+    // increment buffer and size read
+    // reinterpret_cast<AliHLTUInt16_t*>(fBuffer) += 1;
+    fBuffer = (reinterpret_cast<AliHLTUInt16_t*>(fBuffer) + 1);
+    fCurrentSize = sizeof(AliHLTUInt16_t);
+
+    if (fWriteMode)
+    {
+
+      // assign 0 clusters in write mode
+      *fNPreClusters = 0;
+
+    } else {
+
+      // read
+      status = ReadBuffer();
+
+    }
+
   } else {
+
     fCurrentSize = fSize+1;
-    fNPreClusters = NULL;
+    fNPreClusters = 0x0;
+
     if (fWriteMode) {
+
       HLTError("The buffer is too small to store the data block.");
       status = -ENOBUFS;
+
     } else {
+
       HLTError("The buffer is too small to contain the data block.");
       status = -EILSEQ;
+
     }
   }
 
@@ -123,31 +137,40 @@ int AliHLTMUONPreClustersBlock::Reset(void *buffer, AliHLTUInt32_t size, bool wr
 //_________________________________________________________________________________________________
 int AliHLTMUONPreClustersBlock::StartPreCluster(const AliHLTMUONDigitStruct &digit)
 {
-  // start a new precluster
 
+  // start a new precluster
   assert(fWriteMode);
 
-  if (fCurrentSize > fSize4PreCl) {
+  if (fCurrentSize > fSize4PreCl)
+  {
     HLTError("The buffer is too small to store a new precluster.");
-    fLastNDigits = NULL;
-    fLastDigit = NULL;
+    fLastNDigits = 0x0;
+    fLastDigit = 0x0;
     return -ENOBUFS;
   }
 
-  if (fLastNDigits == NULL) fLastNDigits = fNPreClusters + 1;
-  else fLastNDigits = reinterpret_cast<AliHLTUInt16_t*>(fLastDigit + 1);
+  // use current buffer position to store number of digits
+  // and increment buffer
+  fLastNDigits = reinterpret_cast<AliHLTUInt16_t*>( fBuffer );
+  // reinterpret_cast<AliHLTUInt16_t*>( fBuffer ) += 1;
+  fBuffer = (reinterpret_cast<AliHLTUInt16_t*>( fBuffer ) + 1);
+  fCurrentSize += sizeof(AliHLTUInt16_t);
+
+  // assign
   *fLastNDigits = 1;
 
-  fLastDigit = reinterpret_cast<AliHLTMUONDigitStruct*>(fLastNDigits + 1);
-  *fLastDigit = digit;
+  // store digit and increment buffer
+  fLastDigit = reinterpret_cast<AliHLTMUONDigitStruct*>( fBuffer );
+  // reinterpret_cast<AliHLTMUONDigitStruct*>( fBuffer ) += 1;
+  fBuffer = (reinterpret_cast<AliHLTMUONDigitStruct*>( fBuffer ) + 1);
+  fCurrentSize += sizeof(AliHLTMUONDigitStruct);
 
-  *fNPreClusters += 1;
-  fCurrentSize += fgkSizeOfUShort + fgkSizeOfDigit;
+  // increment number of pre-clusters
+  (*fNPreClusters) += 1;
 
-  if (fPreClusters && (fPreClusters->size() > 0 || *fNPreClusters == 1)) {
-    AliHLTMUONPreClusterStruct preCluster = {*fLastNDigits, fLastDigit};
-    fPreClusters->push_back(preCluster);
-  }
+  // insert in list
+  AliHLTMUONPreClusterStruct preCluster = {*fLastNDigits, fLastDigit};
+  fPreClusters.push_back(preCluster);
 
   return 0;
 
@@ -156,115 +179,101 @@ int AliHLTMUONPreClustersBlock::StartPreCluster(const AliHLTMUONDigitStruct &dig
 //_________________________________________________________________________________________________
 int AliHLTMUONPreClustersBlock::AddDigit(const AliHLTMUONDigitStruct &digit)
 {
-  // add a new digit to the current precluster
+
+  /// add a new digit to the current precluster
 
   assert(fWriteMode);
 
-  if (fCurrentSize > fSize4Digit) {
+  if (fCurrentSize > fSize4Digit)
+  {
     HLTError("The buffer is too small to store a new digit.");
-    fLastDigit = NULL;
+    fLastDigit = 0x0;
     return -ENOBUFS;
   }
 
-  if (fLastNDigits == NULL || fLastDigit == NULL) {
+  if (fLastNDigits == 0x0 || fLastDigit == 0x0)
+  {
     HLTError("No precluster to attach the new digit to.");
     return -EPERM;
   }
 
+  // increment number of digits
   *fLastNDigits += 1;
 
-  fLastDigit += 1;
-  *fLastDigit = digit;
+  // assign digit to the buffer and increment buffer
+  *(reinterpret_cast<AliHLTMUONDigitStruct*>( fBuffer )) = digit;
+  // reinterpret_cast<AliHLTMUONDigitStruct*>( fBuffer )+=1;
+  fBuffer = (reinterpret_cast<AliHLTMUONDigitStruct*>( fBuffer ) + 1);
+  fCurrentSize += sizeof(AliHLTMUONDigitStruct);
 
-  fCurrentSize += fgkSizeOfDigit;
-
-  if (fPreClusters && fPreClusters->size() > 0) fPreClusters->back().fNDigits++;
+  // increment number of digits in the stored cluster
+  if( !fPreClusters.empty() ) fPreClusters.back().fNDigits++;
 
   return 0;
 
 }
 
 //_________________________________________________________________________________________________
-const std::vector<AliHLTMUONPreClusterStruct>& AliHLTMUONPreClustersBlock::GetPreClusters()
-{
-  // return the vector of preclusters currently stored in the data block
-
-  if (!fPreClusters) ReadBuffer();
-
-  return *fPreClusters;
-
-}
-
-//_________________________________________________________________________________________________
 int AliHLTMUONPreClustersBlock::ReadBuffer()
 {
+
   /// read the buffer to fill the internal structure
   /// fNPreclus,fNdig,dig1, dig2 ... dign, fNdig, dig1 ... digN, ... last_precluster
-  if (!fPreClusters) {
-    fPreClusters = new std::vector<AliHLTMUONPreClusterStruct>;
-    fPreClusters->reserve(100);
-  }
+  assert( !fWriteMode );
 
-  if (fNPreClusters == NULL) return -EILSEQ;
+  // make sure fNPreClusters was assigned
+  if (fNPreClusters == 0x0) return -EILSEQ;
 
   int status = 0;
+
+  // rolling pre-cluster
   AliHLTMUONPreClusterStruct preCluster;
-  AliHLTUInt32_t currentSize(fgkSizeOfUShort);
-  AliHLTUInt16_t *nDigits(0x0);
-  AliHLTMUONDigitStruct *digit(0x0);
 
-  for (AliHLTUInt16_t i = 0; i < *fNPreClusters; ++i) {
+  // loop over
+  for (AliHLTUInt16_t i = 0; i < *fNPreClusters; ++i)
+  {
 
-    currentSize += fgkSizeOfUShort;
-    if (currentSize < fSize) {
-      if (nDigits == NULL) nDigits = fNPreClusters + 1;
-      else nDigits = reinterpret_cast<AliHLTUInt16_t*>(digit + 1);
+    // store number of digits from buffer and increment
+    AliHLTUInt16_t* nDigits = 0x0;
+    if( fCurrentSize < fSize )
+    {
+
+      nDigits = reinterpret_cast<AliHLTUInt16_t*>(fBuffer);
+      // reinterpret_cast<AliHLTUInt16_t*>(fBuffer)+=1;
+      fBuffer = (reinterpret_cast<AliHLTUInt16_t*>(fBuffer) + 1);
+      fCurrentSize += sizeof( AliHLTUInt16_t );
+
     } else {
+
       HLTError("Cannot read the expected number of preclusters.");
       status = -EILSEQ;
       break;
+
     }
 
-    currentSize += (*nDigits) * fgkSizeOfDigit;
-    if (*nDigits > 0 && currentSize <= fSize) {
-      digit = reinterpret_cast<AliHLTMUONDigitStruct*>(nDigits + 1);
+    // read the digits
+    if( nDigits && (*nDigits) > 0 && fCurrentSize + (*nDigits) * sizeof( AliHLTMUONDigitStruct ) <= fSize )
+    {
+
+      AliHLTMUONDigitStruct* digit = reinterpret_cast<AliHLTMUONDigitStruct*>( fBuffer );
+      // reinterpret_cast<AliHLTMUONDigitStruct*>( fBuffer ) += (*nDigits);
+      fBuffer = (reinterpret_cast<AliHLTMUONDigitStruct*>( fBuffer ) + (*nDigits));
+      fCurrentSize += (*nDigits) * sizeof( AliHLTMUONDigitStruct );
+
+      // store
       preCluster.fNDigits = *nDigits;
       preCluster.fDigits = digit;
-      fPreClusters->push_back(preCluster);
-      digit += (*nDigits) - 1;
+      fPreClusters.push_back(preCluster);
+
     } else {
-      if (*nDigits == 0) {
-        HLTError("The precluster cannot contain 0 digit.");
-      } else {
-        HLTError("Cannot read the expected number of digits.");
-      }
+
+      if (*nDigits == 0) HLTError("The precluster cannot contain 0 digit.");
+      else HLTError("Cannot read the expected number of digits.");
+
       status = -EILSEQ;
       break;
+
     }
-
-  }
-
-  if (fWriteMode) {
-
-    if (currentSize != fCurrentSize && status >= 0) {
-      HLTError("Different number of bytes read than currently written!?");
-      status = -EILSEQ;
-    }
-
-    if (status < 0) fPreClusters->clear();
-
-  } else {
-
-    if (currentSize != fSize && status >= 0) {
-      HLTError("The number of bytes read differs from the buffer size");
-      status = -EILSEQ;
-    }
-
-    if (status < 0) {
-      fCurrentSize = fSize+1;
-      fNPreClusters = NULL;
-      fPreClusters->clear();
-    } else fCurrentSize = currentSize;
 
   }
 
@@ -272,3 +281,26 @@ int AliHLTMUONPreClustersBlock::ReadBuffer()
 
 }
 
+//_________________________________________________________________
+std::ostream& operator << ( std::ostream& stream, const AliHLTMUONPreClusterStruct& cluster )
+{
+  stream << "{fNDigits= " << cluster.fNDigits;
+  for( AliHLTUInt16_t i = 0; i < cluster.fNDigits; ++i )
+  { stream << ", digit[i]= " << cluster.fDigits[i]; }
+  stream << "}";
+
+  return stream;
+}
+
+//_________________________________________________________________
+std::ostream& operator << ( std::ostream& stream, const AliHLTMUONPreClustersBlock& clustersBlock )
+{
+
+  stream << "{fNClusters= " << clustersBlock.GetNPreClusters() << endl;
+  const std::vector<AliHLTMUONPreClusterStruct>& clusters( clustersBlock.GetPreClusters() );
+  for( std::vector<AliHLTMUONPreClusterStruct>::const_iterator iter = clusters.begin(); iter != clusters.end(); ++iter )
+  { stream << "  cluster[i]= " << *iter << std::endl; }
+  stream << "}";
+
+  return stream;
+}
--- HLT/MUON/AliHLTMUONPreClustersBlock.h
+++ HLT/MUON/AliHLTMUONPreClustersBlock.h
@@ -38,13 +38,13 @@ extern "C"
 class AliHLTMUONPreClustersBlock : public AliHLTLogging
 {
 public:
-  
+
   /**
    * Default constructor that produces an object not usable yet.
    * The method Reset(...) must be called to initialize it properly.
    */
   AliHLTMUONPreClustersBlock();
-  
+
   /**
    * Constructor that sets the internal pointer to the start of the buffer
    * space to read/write to and the total size of the buffer in bytes.
@@ -53,12 +53,12 @@ public:
    * @param write   Tell wether the buffer is for writing or reading.
    */
   AliHLTMUONPreClustersBlock(void *buffer, AliHLTUInt32_t size, bool write = false);
-  
+
   /**
    * Destructor to delete internal list of preclusters
    */
   virtual ~AliHLTMUONPreClustersBlock();
-  
+
   /**
    * Method that resets the internal pointer to the start of the new buffer
    * space to read/write to and the total size of this buffer in bytes.
@@ -67,62 +67,90 @@ public:
    * @param write   Tell wether the buffer is for writing or reading.
    */
   int Reset(void *buffer, AliHLTUInt32_t size, bool write = false);
-  
+
   /**
    * Method to fill the buffer with a new precluster and its first digit
    * @param digit  Reference to the first digit to add to the new precluster.
    */
   int StartPreCluster(const AliHLTMUONDigitStruct &digit);
-  
+
   /**
    * Method to add a new digit to the current precluster
    * @param digit  Reference to the digit to add to the current precluster.
    */
   int AddDigit(const AliHLTMUONDigitStruct &digit);
-  
+
   /**
    * Return the number of bytes currently used for the data block in the buffer.
    */
   AliHLTUInt32_t BytesUsed() const {return (fCurrentSize > fSize) ? 0 : fCurrentSize;}
-  
+
   /**
    * Return the number of preclusters currently stored in the data block.
    */
-  AliHLTUInt16_t GetNPreClusters() const {return (fNPreClusters == NULL) ? 0 : *fNPreClusters;}
-  
+  AliHLTUInt16_t GetNPreClusters() const {return (fNPreClusters == 0x0) ? 0 : *fNPreClusters;}
+
   /**
    * Return the vector of preclusters currently stored in the data block.
    */
-  const std::vector<AliHLTMUONPreClusterStruct>& GetPreClusters();
-  
+  const std::vector<AliHLTMUONPreClusterStruct>& GetPreClusters() const
+  { return fPreClusters; }
+
   /**
    * Return the name of the class for log messages.
    */
   virtual const char* Class_Name() { return "AliHLTMUONPreClustersBlock"; }
-  
+
 private:
-  
+
   // Do not allow copying of this class.
+
   /// Not implemented.
   AliHLTMUONPreClustersBlock(const AliHLTMUONPreClustersBlock& /*obj*/);
+
   /// Not implemented.
   AliHLTMUONPreClustersBlock& operator = (const AliHLTMUONPreClustersBlock& /*obj*/);
-  
+
+  /// read the buffer
   int ReadBuffer();
-  
+
   static const AliHLTUInt32_t fgkSizeOfUShort = sizeof(AliHLTUInt16_t);
   static const AliHLTUInt32_t fgkSizeOfDigit = sizeof(AliHLTMUONDigitStruct);
-  
-  AliHLTUInt32_t fSize; ///< total size of the buffer
-  bool fWriteMode; ///< read/write mode
+
+  /// total size of the buffer
+  AliHLTUInt32_t fSize;
+
+  /// read/write mode
+  bool fWriteMode;
   AliHLTUInt32_t fSize4PreCl; ///< total buffer size minus the one resquested to add a new precluster
   AliHLTUInt32_t fSize4Digit; ///< total buffer size minus the one resquested to add a new digit
   AliHLTUInt32_t fCurrentSize; ///< size of the used part of the buffer
-  AliHLTUInt16_t *fNPreClusters; ///< number of preclusters
-  AliHLTUInt16_t *fLastNDigits; ///< number of digits in the last precluster (write mode)
-  AliHLTMUONDigitStruct *fLastDigit; ///< current digit in the last precluster (write mode)
-  std::vector<AliHLTMUONPreClusterStruct> *fPreClusters; ///< list of preclusters
-  
+
+  /// running pointer on the buffer
+  /**
+  indicates where the next chunk of data is written in "write" mode,
+  or where it is read, in "read" mode
+  **/
+  void* fBuffer;
+
+  /// number of preclusters
+  AliHLTUInt16_t *fNPreClusters;
+
+  /// number of digits in the last precluster (write mode)
+  AliHLTUInt16_t *fLastNDigits;
+
+  /// current digit in the last precluster (write mode)
+  AliHLTMUONDigitStruct *fLastDigit;
+
+  /// list of preclusters
+  std::vector<AliHLTMUONPreClusterStruct> fPreClusters;
+
 };
+
+/// stream operator for printout
+std::ostream& operator << ( std::ostream& stream, const AliHLTMUONPreClusterStruct& );
+
+/// stream operator for printout
+std::ostream& operator << ( std::ostream& stream, const AliHLTMUONPreClustersBlock& );
 
 #endif
