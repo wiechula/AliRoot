503b25d162604ec1827f27c0ee7d35748d2aeac4
bogdan <vulpescu@clermont.in2p3.fr>
2016-04-14 11:39:09 +0200

    Adding the MFT for run3 in the Event Display (ed-detector-experts)

--- EVE/EveBase/AliEveInit.cxx
+++ EVE/EveBase/AliEveInit.cxx
@@ -130,10 +130,10 @@ fPath(path)
     {
         if(settings.GetValue(Form("%s.draw",detectorsList[i].c_str()), true))
         {
-            if(detectorsList[i]=="TPC" || detectorsList[i]=="MCH")
+            if(detectorsList[i]=="TPC" || detectorsList[i]=="MCH" || detectorsList[i]=="MFT")
             {
-                // don't load MUON and standard TPC to R-Phi view
-                mv->InitSimpleGeom(geomGentle->GetSimpleGeom((char*)detectorsList[i].c_str()),true,false);
+                // don't load MUON+MFT and standard TPC to R-Phi view
+                mv->InitSimpleGeom(geomGentle->GetSimpleGeom((char*)detectorsList[i].c_str()),false);
             }
             else if(detectorsList[i]=="RPH")
             {
