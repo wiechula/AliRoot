dc8013a8016cb7ef6aca2421c3663f87a609ba31
mkrzewic <mikolaj.krzewicki@cern.ch>
2016-06-29 20:59:10 +0200

    Exit when DISPLAY not set

--- HLT/BASE/util/ZMQhistViewer.cxx
+++ HLT/BASE/util/ZMQhistViewer.cxx
@@ -1,8 +1,14 @@
 #include <TApplication.h>
 #include <TGClient.h>
 #include "AliZMQMTviewerGUI.h"
+#include <cstdlib>
 
 int main(int argc, char **argv) {
+   char* fDISPLAY = std::getenv("DISPLAY");
+   if (!fDISPLAY) {
+     printf("$DISPLAY not set, exiting...\n");
+     return 1;
+   }
    TApplication theApp("App", NULL, NULL);
    AliZMQMTviewerGUI viewer(gClient->GetRoot(), 200, 200, argc, argv);
    if (viewer.GetInitStatus()<0) {
