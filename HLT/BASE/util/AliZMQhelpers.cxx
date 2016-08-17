6a3c630edd3c9eaf8688e65e6724497bae608cdf
mkrzewic <mikolaj.krzewicki@cern.ch>
2016-08-17 15:25:54 +0200

    license the ZMQhelper lib GPLv3

--- HLT/BASE/util/AliZMQhelpers.cxx
+++ HLT/BASE/util/AliZMQhelpers.cxx
@@ -1,5 +1,20 @@
+// a helper library for using ZMQ with ROOT, focussed on multipart messaging
 // blame: Mikolaj Krzewicki, mikolaj.krzewicki@cern.ch
-//see header file for details
+//
+// Copyright (C) 2015 Goethe University Frankfurt
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #include "AliZMQhelpers.h"
