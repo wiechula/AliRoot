365a32bfe55d254b7d80659f106021bf20c82d47
dberzano <dario.berzano@cern.ch>
2016-09-28 13:47:55 +0200

    Improve CTest
    * Affects build only, not runtime

--- test/load_library/LoadLib.C
+++ test/load_library/LoadLib.C
@@ -5,5 +5,13 @@ Int_t LoadLib(const TString &libName)
       libName.Contains("TPHIC"))
     gSystem->Load("libpythia6");
 
+  if (libName.Contains("MONITOR") ||
+      libName.Contains("Eve") ||
+      libName.Contains("HLT") ||
+      libName.Contains("ConverterCollada")) {
+    // Might load system-installed libraries (e.g. libdim.so)
+    gSystem->AddDynamicPath("/lib:/lib64:/usr/lib:/usr/lib64:/usr/local/lib:/usr/local/lib64:/usr/lib/x86_64-linux-gnu");
+  }
+
   return gSystem->Load(libName);
 }
