f06abbcac3d02ffe4bc5788ec579699558c56c71
mkrzewic <mikolaj.krzewicki@cern.ch>
2016-11-03 10:24:45 +0100

    Add AliMergeable virtual class
    this is to provide an interface for Merge(...) and
    GetListOfDrawableObjects() methods to avoid tricks like using
    TObject->Exectute() which is slow (and leaks memory)

---
+++ STEER/STEERBase/AliMergeable.h
@@ -0,0 +1,13 @@
+#ifndef ALIMERGEABLE_H
+#define ALIMERGEABLE_H
+//pure virtual interface class to allow online merging
+//Author: Mikolaj Krzewicki, mkrzewic@cern.ch
+#include "TCollection.h"
+
+class AliMergeable {
+public:
+  virtual Long64_t Merge(TCollection *list) = 0;
+  virtual TCollection* GetListOfDrawableObjects() = 0;
+};
+
+#endif
