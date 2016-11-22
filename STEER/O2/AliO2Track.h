b8d3c4e08a4f4ecce44f896701550fd16ae38ce6
RDeckers <github.com@codingcat.nl>
2016-11-22 18:54:09 +0100

    Tracks now build from external param

--- STEER/O2/AliO2Track.h
+++ STEER/O2/AliO2Track.h
@@ -7,18 +7,21 @@
 #ifndef ALINEWTRACK_H
 #define ALINEWTRACK_H
 
-class AliVParticle;
-
+#include "TMath.h" //Double32_t definition
+class AliVTrack;
+class AliExternalTrackParam;
 // TODO: coordinate system
 /// New Track class, still highly unspecified, is not a ROOT object.
 class AliO2Track {
 public:
   /// Default constructor
-  AliO2Track(float TransverseMomentum = 0, float TransverseMomentumAnglePhi = 0,
-             float TransverseMomentumAngleTheta = 0,
-             float DistanceOfClosestApproachX = 0,
-             float DistanceOfClosestApproachY = 0, float DetectionTime = 0.0f);
-  AliO2Track(const AliVParticle *particle, float DetectionTime = 0.0f);
+  AliO2Track(Double32_t Alpha = 0.0f, Double32_t X = 0.0f, Double32_t Y = 0.0f,
+             Double32_t Z = 0.0f, Double32_t SinPhi = 0.0f,
+             Double32_t TanLambda = 0.0f, Double32_t InversePt = 0.0f,
+             Double32_t *Covariance = nullptr, Double32_t DetectionTime = 0.0f);
+  AliO2Track(const AliVTrack *track, Double32_t DetectionTime = 0.0f);
+  AliO2Track(const AliExternalTrackParam *param,
+             Double32_t DetectionTime = 0.0f);
   /// Destructor
   ~AliO2Track();
 
@@ -30,19 +33,25 @@ private:
   // AliO2Track(const AliO2Track &);
   /// assignment operator prohibited
   // AliO2Track &operator=(const AliO2Track &);
+  void setParameters(const AliExternalTrackParam *param,
+                     Double32_t DetectionTime);
+  void setParameters(Double32_t Alpha, Double32_t X, Double32_t Y, Double32_t Z,
+                     Double32_t SinPhi, Double32_t TanLambda,
+                     Double32_t InversePt, const Double32_t *Covariance,
+                     Double32_t DetectionTime);
 
-  /// Helix specifications.
-  /// TODO: Specify coordinate system
-  // int16_t mPdg = -1;
-  float mTransverseMomentum;
-  float mTransverseMomentumAnglePhi;
-  float mTransverseMomentumAngleTheta;
-  // TODO: do we need these or can we just store the vertex?
-  // float mDistanceOfClosestApproachX;
-  // float mDistanceOfClosestApproachY;
-  // TODO: covariance
-  float covariances[(3 + 2 + 1) + 3 * 2];
-  // float mDetectionTime;
+  Double32_t mDetectionTime;
+  Double32_t mAlpha; // Local <-->global coor.system rotation angle
+  Double32_t mX;     // X coordinate for the point of parametrisation
+
+  Double32_t mY;         /// local Y-coordinate of a track (cm)
+  Double32_t mZ;         /// local Z-coordinate of a track (cm)
+  Double32_t mSinPhi;    /// local sine of the track momentum azimuthal angle
+  Double32_t mTanLambda; /// tangent of the track momentum dip angle
+  Double32_t mInversePt; /// 1/pt (1/(GeV/c)), sign encodes charge.
+  Double32_t
+      mCovariance[15]; /// The track parameter covariance matrix, does not
+                       /// include x, alpha, or DetectionTime.
 };
 
 #endif
