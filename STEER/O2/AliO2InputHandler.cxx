3e2f7566374501720557fefdbcffb35ec050c9fc
RDeckers <git@codingcat.nl>
2016-12-02 19:17:19 +0100

    Starting work on input handler

---
+++ STEER/O2/AliO2InputHandler.cxx
@@ -0,0 +1,20 @@
+/// \file AliO2InputHandler.cxx
+/// \brief implementation of the AliO2InputHandler class.
+/// \since 2016-11-28
+/// \author R.G.A. Deckers
+/// \copyright
+///  This program is free software; you can redistribute it and/or
+/// modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation; either version 3 of
+/// the License, or (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but
+/// WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+/// General Public License for more details at
+/// https://www.gnu.org/copyleft/gpl.html
+///
+
+#include "AliO2InputHandler.h"
+
+// root specific
+ClassImp(AliO2InputHandler);
