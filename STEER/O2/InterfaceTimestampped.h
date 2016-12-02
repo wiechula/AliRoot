b246eb44f2abae790df20c928d813273fec97bf8
RDeckers <git@codingcat.nl>
2016-12-02 19:12:05 +0100

    Remove empy implementation

--- STEER/O2/InterfaceTimestampped.h
+++ STEER/O2/InterfaceTimestampped.h
@@ -2,7 +2,16 @@
 /// \author R.G.A. Deckers
 /// \brief Definition of the InterfaceTimestampped class.
 ///
-/// See implementation file for copyright details.
+/// \copyright
+///  This program is free software; you can redistribute it and/or
+/// modify it under the terms of the GNU General Public License as
+/// published by the Free Software Foundation; either version 3 of
+/// the License, or (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but
+/// WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+/// General Public License for more details at
+/// https://www.gnu.org/copyleft/gpl.html
 
 #ifndef INTERFACETIMESTAMPPED_H
 #define INTERFACETIMESTAMPPED_H
