7241db13d147ecf8773072905d1ac349fd686c4a
RDeckers <git@codingcat.nl>
2017-02-07 14:35:42 +0100

    AoS vs SoA documented example

---
+++ O2/Examples/AoS_vs_SoA/README.md
@@ -0,0 +1,49 @@
+# Comparison of array-of-struct vs. struct-of-arrays for data analysis
+
+## description
+This example tries to estimate the performance difference between storing data in a SoA or a AoS method.
+
+It does so by running an arbitrary analysis over generated data. This analysis can be summarized as:
+```
+sum = 0
+foreach track:
+ if (uint32_t)track.some_value > cutoff {
+   r = sqrt(track.x*track.x+track.y*track.y+track.z*track.z);
+   sum += track.pt - r;
+ }
+ return sum;
+ ```
+Here `some_value` is an arbitrary type to cut on, it is referred to as a `muonScore` in the code because it could be, for example, some precomputed value describing how likely it is that a given track describes a muon.
+
+There are three implementations that are tested: SoA, AoS, and a vectorized SoA implementation. All versions support threading via OpenMP.
+
+Different values for `cutoff` are tested and for each value the analysis is repeated 11 times in a row to obtain better results. The default size of the data is large enough that caching should have no effect on the results.
+
+## requirements
+The vectorized version uses AVX2 instructions, therefore it is required to have a processor capable of executing these instructions (Intel Haswell or later, or the equivalent AMD).
+
+The program allocates a significant amount of memory so at least 8 GiB of ram are recommended. If this is not available the memory used can be reduced by tweaking the variable `count` inside the `main` function.
+
+**note:** if you reduce `count` too much, the data will still be stored in the cache between iterations. If this is not desired there is a function `flush_cache()` which can be used in between iterations to remove the data from the cache. This function does increase the run-time by a significant amount.
+
+## running
+In order to build the examples run
+```
+  clang++ -fopenmp --std=c++11 -O3 -march=native src/*.cxx main.cxx -o comparison
+```
+or replace `clang++` by `g++`.
+For the best measurement results it is recommended, but not necessary, to run the binary in real-time mode to reduce any influence from other processes. On Linux this can be done by running:
+```
+sudo chrt 99 ./comparison
+```
+**Note:** this will cause all other programs to almost certainly freeze up for the duration of the comparison. This is expected behavior. This should only take a few seconds.
+
+ Output is of the form `probability result minimum median maximum bandwidth` for each version, where: `probability` is the odds of a track passing the initial filter, `result` is the computed sum (included only to prevent optimizations from removing it), `minimum` is the shortest time taken of all iterations (in ns), `median` is the median time, `maximum` is the maximum time, `bandwidth` is the predicted effective bandwidth in GB/s (4 bytes for each track that does not pass the filter and sizeof(Track) bytes for each track that does, divided by the `minimum` time).
+
+## expected results
+The following results were obtained on an i7-4750HQ based system with a theoretical max bandwidth of 25.6 GB/s.
+![](media/bandwidth.png "Measured effective bandwidth")
+The dip near the start can be explained by the access pattern, where a lot of data gets pulled in from other cache-lines when a track passes the filter, but are not likely to pass the filter themselves resulting in wasted bandwidth.
+
+ Performance peaks at both extremes, in the case of the probability going to one this is due to the fact that all data loaded is used and is loaded in linear fashion.
+In the case of the probablity going to zero most tracks will fail the filter and in the SoA case this means the program only scans a single flat-array. In the AoS the filter parameter is not packed together and thus part of the loaded data (from the same cache-line) is wasted bytes. (the difference matches the expected results: a flat track in this example is 20 bytes, 4 of which are the filter values therefore we expect a difference on the order of 5 times).
---
+++ O2/Examples/AoS_vs_SoA/main.cxx
@@ -0,0 +1,111 @@
+#include "headers/AoS_vs_SoA.h"
+#include <algorithm>
+#include <array>
+#include <chrono>
+
+int main() {
+  constexpr unsigned number_of_tracks = 1 << 26;
+  constexpr int max_one_in = 1 << 24;      // 1<<24 ~= one in 17 million
+  constexpr int repetitions_per_test = 11; // run each the analysis this many
+                                           // times in a row to get some
+                                           // statistically sound results.
+  std::array<double, repetitions_per_test>
+      timings; // Array for storing our timing results.
+
+  auto tracks = createTrackContainer(number_of_tracks);
+  puts("\"SoA (Vectorized)\"");
+  // loop over different odds of passing a filter to simulate cutting on
+  // relatively few variables.
+  for (int one_in = 1; one_in <= max_one_in; one_in += (one_in + 1) / 2) {
+    double result = 0; // we need to get and use the result from each run to
+                       // prevent the compiler from optimizing it out.
+
+    // repeat several times to get better timing information
+    for (int run = 0; run < repetitions_per_test; run++) {
+      auto start_time = std::chrono::steady_clock::now();
+      result += analyseMuons_v(tracks, number_of_tracks, one_in);
+      auto end_time = std::chrono::steady_clock::now();
+      timings[run] =
+          std::chrono::duration<double, std::nano>(end_time - start_time)
+              .count(); // add the timing information, in ns, to the timings
+                        // aray
+    }
+    // get the fastest, median, and slowest run time.
+    std::sort(timings.begin(), timings.end());
+    double fastest = timings[0];
+    double slowest = timings[repetitions_per_test - 1];
+    double median = timings[repetitions_per_test / 2];
+    // Compute the efective bandwidth achieved
+    size_t size_filter_array = number_of_tracks * 4;
+    size_t size_passing_tracks =
+        (sizeof(FlatTrack::flat_type) - 4) * number_of_tracks /
+        one_in; // subtract 4 to prevent double counting the filter variable.
+    double highest_bandwith =
+        (size_filter_array + size_passing_tracks) / fastest;
+    double pass_rate = 1.0 / one_in;
+    // print the results.
+    printf("%.4e\t%.4e %.4e %.4e\t%6.2f\t%2.1e\n", pass_rate, slowest, median,
+           fastest, highest_bandwith, result);
+  }
+  puts("\n");
+
+  puts("\"SoA (Scalar)\"");
+  for (int one_in = 1; one_in <= max_one_in; one_in += (one_in + 1) / 2) {
+    double result = 0; // we need to get and use the result from each run to
+                       // prevent the compiler from optimizing it out.
+    for (int run = 0; run < repetitions_per_test; run++) {
+      auto start_time = std::chrono::steady_clock::now();
+      result += analyseMuons(tracks, number_of_tracks, one_in);
+      auto end_time = std::chrono::steady_clock::now();
+      timings[run] =
+          std::chrono::duration<double, std::nano>(end_time - start_time)
+              .count();
+    }
+    std::sort(timings.begin(), timings.end());
+    double fastest = timings[0];
+    double slowest = timings[repetitions_per_test - 1];
+    double median = timings[repetitions_per_test / 2];
+    size_t size_filter_array = number_of_tracks * 4;
+    size_t size_passing_tracks =
+        (sizeof(FlatTrack::flat_type) - 4) * number_of_tracks /
+        one_in; // subtract 4 to prevent double counting the filter variable.
+    double highest_bandwith =
+        (size_filter_array + size_passing_tracks) / fastest;
+    double pass_rate = 1.0 / one_in;
+    printf("%.4e\t%.4e %.4e %.4e\t%6.2f\t%2.1e\n", pass_rate, slowest, median,
+           fastest, highest_bandwith, result);
+  }
+  puts("\n");
+
+  freeTrackContainer(tracks);
+  auto tracks_flat = make_flat(number_of_tracks);
+  puts("\"AoS (Scalar)\"");
+  for (int one_in = 1; one_in <= max_one_in; one_in += (one_in + 1) / 2) {
+    double result = 0; // we need to get and use the result from each run to
+                       // prevent the compiler from optimizing it out.
+    for (int run = 0; run < repetitions_per_test; run++) {
+      auto start_time = std::chrono::steady_clock::now();
+      result += analyseMuons_flat(tracks_flat, number_of_tracks, one_in);
+      auto end_time = std::chrono::steady_clock::now();
+      timings[run] =
+          std::chrono::duration<double, std::nano>(end_time - start_time)
+              .count();
+    }
+    std::sort(timings.begin(), timings.end());
+    double fastest = timings[0];
+    double slowest = timings[repetitions_per_test - 1];
+    double median = timings[repetitions_per_test / 2];
+    size_t size_filter_array = number_of_tracks * 4;
+    size_t size_passing_tracks =
+        (sizeof(FlatTrack::flat_type) - 4) * number_of_tracks /
+        one_in; // subtract 4 to prevent double counting the filter variable.
+    double highest_bandwith =
+        (size_filter_array + size_passing_tracks) / fastest;
+    double pass_rate = 1.0 / one_in;
+    printf("%.4e\t%.4e %.4e %.4e\t%6.2f\t%2.1e\n", pass_rate, slowest, median,
+           fastest, highest_bandwith, result);
+  }
+  puts("\n");
+
+  return 0;
+}
