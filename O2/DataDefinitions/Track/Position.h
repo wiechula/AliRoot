786592020752d01d7cf3e66b0cc426f7c091171e
RDeckers <roel.deckers@cern.ch>
2017-03-07 19:14:20 +0100

    No longer used

--- O2/DataDefinitions/Track/Position.h
+++
@@ -1,9 +0,0 @@
-#ifndef TF_DD_TRACK_POSITION_H
-#define TF_DD_TRACK_POSITION_H
-
-#include "Position/Spatial.h"
-#include "Position/Temporal.h"
-
-DEFINE_MULTI(Position, ITrack) { MULTI_BODY(Position); };
-using PositionAll = Position<Temporal, SpatialAll>;
-#endif
