429c11764fe244985b5715c6799cd257b71c56dd
drohr <drohr@cern.ch>
2017-03-22 19:13:49 +0100

    add sort option to cluster stat component

--- HLT/TPCLib/comp/AliHLTTPCClusterStatComponent.h
+++ HLT/TPCLib/comp/AliHLTTPCClusterStatComponent.h
@@ -77,6 +77,7 @@ class AliHLTTPCClusterStatComponent : public AliHLTProcessor, public AliOptionPa
   int fPrintClustersScaled; //!
   int fDumpClusters; //!
   int fAggregate; //!
+  int fSort; //!
   int fEvent;
   
   FILE* fp;
